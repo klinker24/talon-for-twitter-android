d1e2a662344a8527ddef811662c673ad9b8a6fb7
Luke Klinker <lkinker1@gmail.com>
2013-11-28 10:23:29 -0600

    changed the icon for get help since i use that for trends

--- src/com/klinker/android/talon/adapters/MainDrawerArrayAdapter.java
+++ src/com/klinker/android/talon/adapters/MainDrawerArrayAdapter.java
@@ -106,7 +106,7 @@ public class MainDrawerArrayAdapter extends ArrayAdapter<String> {
             a.recycle();
             holder.icon.setImageResource(resource);
         } else if (text.get(position).equals(context.getResources().getString(R.string.trends))) {
-            TypedArray a = context.getTheme().obtainStyledAttributes(new int[]{R.attr.getHelp});
+            TypedArray a = context.getTheme().obtainStyledAttributes(new int[]{R.attr.worldTrends});
             int resource = a.getResourceId(0, 0);
             a.recycle();
             holder.icon.setImageResource(resource);
