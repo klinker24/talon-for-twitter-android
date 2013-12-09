4d1e203aa282b7d03d23261ad29412d5e9dcf83a
lukeklinker <lklinker1@gmail.com>
2013-12-08 20:41:59 -0600

    derp

--- src/com/klinker/android/talon/ui/drawer_activities/lists/ListsActivity.java
+++ src/com/klinker/android/talon/ui/drawer_activities/lists/ListsActivity.java
@@ -215,7 +215,7 @@ public class ListsActivity extends DrawerActivity {
 
         overridePendingTransition(0,0);
         finish();
-        Intent restart = new Intent(context, ListActivity.class);
+        Intent restart = new Intent(context, ListsActivity.class);
         restart.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         overridePendingTransition(0, 0);
         startActivity(restart);
