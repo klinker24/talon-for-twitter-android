ab381b49f2119bf5219780064676123a381eb63e
lukeklinker <lklinker1@gmail.com>
2013-12-10 11:00:58 -0600

    another

--- src/com/klinker/android/talon/ui/UserProfileActivity.java
+++ src/com/klinker/android/talon/ui/UserProfileActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
