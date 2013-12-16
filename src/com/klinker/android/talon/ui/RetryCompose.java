a36d8050d6e25f61f83370fc506852f15828bbfd
lukeklinker <lklinker1@gmail.com>
2013-12-15 22:24:28 -0600

    clears failed notification after clicking it

--- src/com/klinker/android/talon/ui/RetryCompose.java
+++ src/com/klinker/android/talon/ui/RetryCompose.java
@@ -1,4 +1,14 @@
 package com.klinker.android.talon.ui;
 
+import android.app.NotificationManager;
+import android.content.Context;
+
 public class RetryCompose extends ComposeActivity {
+
+    @Override
+    public void onStart() {
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(5); // failed option
+    }
 }
