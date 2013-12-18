daddc1a158b975d5330d58757839f91e9e1f152c
lukeklinker <lklinker1@gmail.com>
2013-12-18 11:00:47 -0600

    re-wrote the compose activities to use a standard superclass.
    also fixed the location not being attached to statuses... lol oops

--- src/com/klinker/android/talon/ui/ComposeDMActivity.java
+++ src/com/klinker/android/talon/ui/ComposeDMActivity.java
@@ -1,34 +1,30 @@
 package com.klinker.android.talon.ui;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.klinker.android.talon.R;
+import com.klinker.android.talon.ui.widgets.QustomDialogBuilder;
 import com.klinker.android.talon.utils.Utils;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
-public class ComposeDMActivity extends ComposeActivity {
-
-    private EditText contactEntry;
-
+public class ComposeDMActivity extends Compose {
 
     public void setUpLayout() {
-        setContentView(R.layout.compose_activity);
+        setContentView(R.layout.compose_dm_activity);
 
-        LinearLayout buttons = (LinearLayout) findViewById(R.id.buttons);
-        buttons.setVisibility(View.GONE);
-
-        LinearLayout overflow = (LinearLayout) findViewById(R.id.overflow);
-        overflow.setVisibility(View.GONE);
+        setUpSimilar();
 
         contactEntry = (EditText) findViewById(R.id.contact_entry);
         contactEntry.setVisibility(View.VISIBLE);
@@ -36,30 +32,39 @@ public class ComposeDMActivity extends ComposeActivity {
         String screenname = getIntent().getStringExtra("screenname");
 
         if (screenname != null) {
-            contactEntry.setText(screenname);
+            contactEntry.setText("@" + screenname);
             contactEntry.setSelection(contactEntry.getText().toString().length());
         }
 
-        final TextView charRemaining = (TextView) findViewById(R.id.char_remaining);
-        final EditText reply = (EditText) findViewById(R.id.tweet_content);
-
-        charRemaining.setText(140 - reply.getText().length() + "");
-        reply.addTextChangedListener(new TextWatcher() {
+        Button at = (Button) findViewById(R.id.at_button);
+        at.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            public void onClick(View view) {
+                final QustomDialogBuilder qustomDialogBuilder = new QustomDialogBuilder(context, sharedPrefs.getInt("current_account", 1)).
+                        setTitle(getResources().getString(R.string.type_user)).
+                        setTitleColor(getResources().getColor(R.color.app_color)).
+                        setDividerColor(getResources().getColor(R.color.app_color));
 
-            }
+                qustomDialogBuilder.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-                charRemaining.setText(140 - reply.getText().length() + "");
-            }
+                qustomDialogBuilder.setPositiveButton(getResources().getString(R.string.add_user), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        contactEntry.append(qustomDialogBuilder.text.getText().toString());
+                    }
+                });
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+                qustomDialogBuilder.show();
 
+                overflow.performClick();
             }
         });
+
     }
 
     public boolean doneClick() {
@@ -86,58 +91,4 @@ public class ComposeDMActivity extends ComposeActivity {
         new SendDirectMessage().execute(status);
     }
 
-    private class SendDirectMessage extends AsyncTask<String, String, Boolean> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-        protected Boolean doInBackground(String... args) {
-            String status = args[0];
-            try {
-                Twitter twitter = Utils.getTwitter(getApplicationContext());
-
-                Log.v("screen_name", contactEntry.getText().toString());
-
-                twitter.sendDirectMessage(contactEntry.getText().toString(), status);
-
-                return true;
-
-            } catch (TwitterException e) {
-                e.printStackTrace();
-            }
-
-            return false;
-        }
-
-        protected void onPostExecute(Boolean sent) {
-            // dismiss the dialog after getting all products
-
-            if (sent) {
-            // updating ui from Background Thread
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getBaseContext(),
-                                getApplicationContext().getResources().getString(R.string.direct_message_sent), Toast.LENGTH_SHORT)
-                                .show();
-                        // Clearing EditText field
-                    }
-                });
-            } else {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getBaseContext(),
-                                getResources().getString(R.string.error), Toast.LENGTH_SHORT)
-                                .show();
-                        // Clearing EditText field
-                    }
-                });
-            }
-        }
-
-    }
 }
