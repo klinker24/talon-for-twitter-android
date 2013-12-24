f921408d266d81d68dfff613ebe2ecbfd9169cbf
lukeklinker <lklinker1@gmail.com>
2013-12-24 15:02:42 -0600

    fixes for the package rename

--- src/com/klinker/android/twitter/data/sq_lite/HomeContentProvider.java
+++ src/com/klinker/android/twitter/data/sq_lite/HomeContentProvider.java
@@ -18,7 +18,7 @@ import twitter4j.Status;
 public class HomeContentProvider extends ContentProvider {
     static final String TAG = "HomeTimeline";
 
-    public static final String AUTHORITY = "com.klinker.android.talon.provider";
+    public static final String AUTHORITY = "com.klinker.android.twitter.provider";
     static final String BASE_PATH = "tweet_id";
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
 
