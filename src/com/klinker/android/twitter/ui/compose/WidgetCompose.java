7ec59509b39fdc820626b4f0ed078eb5e077aeb2
lukeklinker <lklinker1@gmail.com>
2014-01-02 23:05:44 -0600

    todo

--- src/com/klinker/android/twitter/ui/compose/WidgetCompose.java
+++ src/com/klinker/android/twitter/ui/compose/WidgetCompose.java
@@ -2,9 +2,6 @@ package com.klinker.android.twitter.ui.compose;
 
 import android.os.Bundle;
 
-/**
- * Created by luke on 1/2/14.
- */
 public class WidgetCompose extends ComposeActivity {
 
     @Override
