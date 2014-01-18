6cf051e7322bac821e395e020f3a48a10c525526
lukeklinker <lklinker1@gmail.com>
2014-01-17 18:35:00 -0600

    widget will update better

--- src/com/klinker/android/twitter/services/TalonPullNotificationService.java
+++ src/com/klinker/android/twitter/services/TalonPullNotificationService.java
@@ -364,6 +364,7 @@ public class TalonPullNotificationService extends Service {
 
                     mContext.sendBroadcast(new Intent("com.klinker.android.twitter.NEW_TWEET"));
                     mContext.sendBroadcast(new Intent("com.klinker.android.twitter.UPDATE_NOTIF"));
+                    mContext.sendBroadcast(new Intent("com.klinker.android.talon.UPDATE_WIDGET"));
 
                     sharedPreferences.edit().putBoolean("refresh_me", true).commit();
                     sharedPreferences.edit().putLong("second_last_tweet_id_" + sharedPreferences.getInt("current_account", 1), home.getLastId(sharedPreferences.getInt("current_account", 1)));
