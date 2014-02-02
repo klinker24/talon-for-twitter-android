29b901c91a0ce373abdaa25f2e79dc0b0bdd8d59
Luke Klinker <lkinker1@gmail.com>
2014-02-02 14:19:14 -0600

    fixed albums in imgur

--- src/com/klinker/android/twitter/utils/HtmlUtils.java
+++ src/com/klinker/android/twitter/utils/HtmlUtils.java
@@ -363,7 +363,7 @@ public class HtmlUtils {
                 } else if (exp.toLowerCase().contains("imgur")) {
                     int start = exp.indexOf(".com/") + 6;
                     imageUrl = "http://i.imgur.com/" + exp.substring(start) + "m.jpg" ;
-                    imageUrl = imageUrl.replace("gallery/", "").replace("a/", "");
+                    imageUrl = imageUrl.replace("gallery/", "").replace("a/", ""); 
                     otherUrl += exp + "  ";
                 } else if (exp.toLowerCase().contains("pbs.twimg.com")) {
                     imageUrl = exp;
