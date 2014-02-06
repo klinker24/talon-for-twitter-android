da280e47464d444a0abca6a67f448ef05673c7be
lukeklinker <lklinker1@gmail.com>
2014-02-06 10:20:59 -0600

    changes to downloading a picture

--- src/com/klinker/android/twitter/ui/widgets/PhotoViewerDialog.java
+++ src/com/klinker/android/twitter/ui/widgets/PhotoViewerDialog.java
@@ -148,7 +148,13 @@ public class PhotoViewerDialog extends Activity {
 
                             URL mUrl = new URL(url);
 
-                            Bitmap bitmap = BitmapFactory.decodeStream(mUrl.openConnection().getInputStream());
+                            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+                            InputStream is = new BufferedInputStream(conn.getInputStream());
+
+                            BitmapFactory.Options options = new BitmapFactory.Options();
+                            options.inJustDecodeBounds = false;
+
+                            Bitmap bitmap = decodeSampledBitmapFromResourceMemOpt(is, 600, 600);
 
                             Random generator = new Random();
                             int n = 1000000;
@@ -185,6 +191,70 @@ public class PhotoViewerDialog extends Activity {
                 finish();
             }
         });
+    }
+
+    public Bitmap decodeSampledBitmapFromResourceMemOpt(
+            InputStream inputStream, int reqWidth, int reqHeight) {
+
+        byte[] byteArr = new byte[0];
+        byte[] buffer = new byte[1024];
+        int len;
+        int count = 0;
+
+        try {
+            while ((len = inputStream.read(buffer)) > -1) {
+                if (len != 0) {
+                    if (count + len > byteArr.length) {
+                        byte[] newbuf = new byte[(count + len) * 2];
+                        System.arraycopy(byteArr, 0, newbuf, 0, count);
+                        byteArr = newbuf;
+                    }
+
+                    System.arraycopy(buffer, 0, byteArr, count, len);
+                    count += len;
+                }
+            }
+
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeByteArray(byteArr, 0, count, options);
+
+            options.inSampleSize = calculateInSampleSize(options, reqWidth,
+                    reqHeight);
+            options.inPurgeable = true;
+            options.inInputShareable = true;
+            options.inJustDecodeBounds = false;
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+            return BitmapFactory.decodeByteArray(byteArr, 0, count, options);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options opt, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = opt.outHeight;
+        final int width = opt.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     private class DownloadPic extends AsyncTask<String, Void, Bitmap> {
