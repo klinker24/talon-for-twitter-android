63abbf211a0cf473b46e9bbd4ffdc5400933b5f3
lukeklinker <lklinker1@gmail.com>
2014-02-11 22:24:03 -0600

    set up the pager strip title

--- src/com/klinker/android/twitter/settings/configure_pages/ConfigurationPagerAdapter.java
+++ src/com/klinker/android/twitter/settings/configure_pages/ConfigurationPagerAdapter.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.support.v13.app.FragmentPagerAdapter;
 
+import com.klinker.android.twitter.R;
 import com.klinker.android.twitter.settings.configure_pages.fragments.ExampleHomeFragment;
 import com.klinker.android.twitter.settings.configure_pages.fragments.PageOneFragment;
 import com.klinker.android.twitter.settings.configure_pages.fragments.PageTwoFragment;
@@ -12,8 +13,11 @@ import com.klinker.android.twitter.settings.configure_pages.fragments.PageTwoFra
 
 public class ConfigurationPagerAdapter extends FragmentPagerAdapter {
 
+    private Context context;
+
     public ConfigurationPagerAdapter(FragmentManager manager, Context context) {
         super(manager);
+        context = context;
     }
 
     @Override
@@ -34,5 +38,18 @@ public class ConfigurationPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 3; // 3 pages
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return context.getResources().getString(R.string.page_1);
+            case 1:
+                return context.getResources().getString(R.string.page_2);
+            case 2:
+                return context.getResources().getString(R.string.timeline);
+        }
+        return null;
     }
 }
