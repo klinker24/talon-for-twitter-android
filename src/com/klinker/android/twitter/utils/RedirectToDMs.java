9c12499ff27347e86adea447f1001760fa47a5b4
lukeklinker <lklinker1@gmail.com>
2014-02-22 09:28:01 -0600

    added a few activities to redirect the notification clicks to mentions and dms

---
+++ src/com/klinker/android/twitter/utils/RedirectToDMs.java
@@ -0,0 +1,44 @@
+package com.klinker.android.twitter.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import com.klinker.android.twitter.settings.AppSettings;
+import com.klinker.android.twitter.ui.MainActivity;
+
+/**
+ * Created by luke on 2/22/14.
+ */
+public class RedirectToDMs extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        int currentAccount = sharedPrefs.getInt("current_account", 1);
+
+        int page1Type = sharedPrefs.getInt("account_" + currentAccount + "_page_1", AppSettings.PAGE_TYPE_NONE);
+        int page2Type = sharedPrefs.getInt("account_" + currentAccount + "_page_2", AppSettings.PAGE_TYPE_NONE);
+
+        int extraPages = 0;
+        if (page1Type != AppSettings.PAGE_TYPE_NONE) {
+            extraPages++;
+        }
+
+        if (page2Type != AppSettings.PAGE_TYPE_NONE) {
+            extraPages++;
+        }
+
+        Intent mentions = new Intent(this, MainActivity.class);
+        mentions.putExtra("page_to_open", 2 + extraPages);
+
+        finish();
+
+        startActivity(mentions);
+    }
+}
