e99258393c0ea8dedcde84587af769fbf86a57fa
lukeklinker <lklinker1@gmail.com>
2014-02-24 13:03:02 -0600

    local trend locations added

---
+++ src/com/klinker/android/twitter/utils/LocalTrendsUtils.java
@@ -0,0 +1,334 @@
+package com.klinker.android.twitter.utils;
+
+/**
+ * Created by luke on 2/24/14.
+ */
+public class LocalTrendsUtils {
+
+    public static String[][] argentina = {
+            {"All Cities", "23424747"},
+            {"Buenos Aires", "468739"}
+    };
+
+    public static String[][] australia = {
+            {"All Cities", "23424748"},
+            {"Sydney", "1105779"}
+    };
+
+    public static String[][] brazil = {
+            {"Brasília", "455819"},
+            {"Rio de Janeiro", "455825"},
+            {"Salvador", "455826"},
+            {"São Paulo", "455827"},
+            {"All Cities", "23424768"},
+            {"Fortaleza", "455830"},
+            {"Manaus", "455833"}
+    };
+
+    public static String[][] canada = {
+            {"All Cities", "23424775"},
+            {"Montreal", "3534"},
+            {"Vancouver", "9807"},
+            {"Toronto", "4118"}
+    };
+
+    public static String[][] chile = {
+            {"All Cities", "23424782"},
+            {"Santiago", "349859"}
+    };
+
+    public static String[][] colombia = {
+            {"All Cities", "23424787"},
+            {"Bogotá", "368148"}
+    };
+
+    public static String[][] dominican_republic = {
+            {"Santo Domingo", "76456"},
+            {"All Cities", "23424800"}
+    };
+
+    public static String[][] ecuador = {
+            {"All Cities", "23424801"}
+    };
+
+    public static String[][] france = {
+            {"Paris", "615702"},
+            {"All Cities", "23424819"},
+            {"Marseille", "610264"},
+            {"Lyon", "609125"}
+    };
+
+    public static String[][] germany = {
+            {"Berlin", "638242"},
+            {"All Cities", "23424829"},
+            {"Hamburg", "656958"},
+            {"Munich", "676757"}
+    };
+
+    public static String[][] guatemala = {
+            {"All Cities", "23424834"}
+    };
+
+    public static String[][] india = {
+            {"Bangalore", "2295420"},
+            {"Chennai", "2295424"},
+            {"Delhi", "20070458"},
+            {"All Cities", "23424848"},
+            {"Ahmedabad", "2295402"},
+            {"Mumbai", "2295411"},
+            {"Hyderabad", "2295414"}
+    };
+
+    public static String[][] indonesia = {
+            {"Jakarta", "1047378"},
+            {"Bandung", "1047180"},
+            {"All Cities", "23424846"},
+            {"Bekasi", "1030077"},
+            {"Surabaya", "1044316"}
+    };
+
+    public static String[][] ireland = {
+            {"Dublin", "560743"},
+            {"All Cities", "23424803"}
+    };
+
+    public static String[][] italy = {
+            {"All Cities", "23424853"}
+    };
+
+    public static String[][] japan = {
+            {"Sendai", "1118129"},
+            {"Osaka", "15015370"},
+            {"Kyoto", "15015372"},
+            {"Tokyo", "1118370"},
+            {"Fukuoka", "1117099"},
+            {"Sapporo", "1118108"},
+            {"All Cities", "23424856"},
+            {"Takamatsu", "1118285"},
+            {"Nagoya", "1117817"},
+            {"Okinawa", "2345896"}
+    };
+
+    public static String[][] malaysia = {
+            {"Kuala Lumpur", "1154781"},
+            {"Klang", "1154726"},
+            {"All Cities", "23424901"}
+    };
+
+    public static String[][] mexico = {
+            {"Monterrey", "134047"},
+            {"All Cities", "23424900"},
+            {"Mexico City", "116545"}
+    };
+
+    public static String[][] netherlands = {
+            {"All Cities", "23424909"},
+            {"Amsterdam", "727232"},
+            {"Den Haag", "726874"},
+            {"Rotterdam", "733075"}
+    };
+
+    public static String[][] new_zealand = {
+            {"All Cities", "23424916"}
+    };
+
+    public static String[][] nigeria = {
+            {"All Cities", "23424908"},
+            {"Lagos", "1398823"}
+    };
+
+    public static String[][] pakistan = {
+            {"All Cities", "23424922"}
+    };
+
+    public static String[][] peru = {
+            {"All Cities", "23424919"},
+            {"Lima", "418440"}
+    };
+
+    public static String[][] philippines = {
+            {"Manila", "1199477"},
+            {"Quezon City", "1199682"},
+            {"All Cities", "23424934"}
+    };
+
+    public static String[][] russia = {
+            {"Moscow", "2122265"},
+            {"Novosibirsk", "2122541"},
+            {"All Cities", "23424936"},
+            {"Saint Petersburg", "2123260"}
+    };
+
+    public static String[][] singapore = {
+            {"All Cities", "23424948"}
+    };
+
+    public static String[][] south_africa = {
+            {"All Cities", "23424942"},
+            {"Johannesburg", "1582504"}
+    };
+
+    public static String[][] spain = {
+            {"All Cities", "23424950"},
+            {"Barcelona", "753692"},
+            {"Madrid", "766273"}
+    };
+
+    public static String[][] sweden = {
+            {"All Cities", "23424954"},
+            {"Stockholm", "906057"}
+    };
+
+    public static String[][] turkey = {
+            {"Ankara", "2343732"},
+            {"Istanbul", "2344116"},
+            {"Izmir", "2344117"},
+            {"All Cities", "23424969"}
+    };
+
+    public static String[][] united_arab_emirates = {
+            {"All Cities", "23424738"}
+    };
+
+    public static String[][] united_kingdom = {
+            {"All Cities", "23424975"},
+            {"Manchester", "28218"},
+            {"Glasgow", "21125"},
+            {"Birmingham", "12723"},
+            {"London", "44418"}
+    };
+
+    public static String[][] united_states = {
+            {"Miami", "2450022"},
+            {"New Orleans", "2458833"},
+            {"Salt Lake City", "2487610"},
+            {"Columbus", "2383660"},
+            {"Providence", "2477058"},
+            {"All Cities", "23424977"},
+            {"Chicago", "2379574"},
+            {"Houston", "2424766"},
+            {"Washington", "2514815"},
+            {"Portland", "2475687"},
+            {"Sacramento", "2486340"},
+            {"Jackson", "2428184"},
+            {"Memphis", "2449323"},
+            {"Tampa", "2503863"},
+            {"Austin", "2357536"},
+            {"St. Louis", "2486982"},
+            {"Raleigh", "2478307"},
+            {"Dallas-Ft. Worth", "2388929"},
+            {"San Diego", "2487889"},
+            {"San Francisco", "2487956"},
+            {"Minneapolis", "2452078"},
+            {"Nashville", "2457170"},
+            {"New Haven", "2458410"},
+            {"New York", "2459115"},
+            {"Las Vegas", "2436704"},
+            {"Norfolk", "2460389"},
+            {"Denver", "2391279"},
+            {"Cincinnati", "2380358"},
+            {"Harrisburg", "2418046"},
+            {"Baton Rouge", "2359991"},
+            {"Baltimore", "2358820"},
+            {"Richmond", "2480894"},
+            {"San Antonio", "2487796"},
+            {"Pittsburgh", "2473224"},
+            {"Orlando", "2466256"},
+            {"Detroit", "2391585"},
+            {"Philadelphia", "2471217"},
+            {"Tallahassee", "2503713"},
+            {"Greensboro", "2414469"},
+            {"Indianapolis", "2427032"},
+            {"Milwaukee", "2451822"},
+            {"Charlotte", "2378426"},
+            {"Seattle", "2490383"},
+            {"Cleveland", "2381475"},
+            {"Phoenix", "2471390"},
+            {"Birmingham", "2364559"},
+            {"Boston", "2367105"},
+            {"Los Angeles", "2442047"},
+            {"Atlanta", "2357024"}
+    };
+
+    public static String[][] venezuela = {
+            {"Caracas", "395269"},
+            {"Maracaibo", "395270"},
+            {"Valencia", "395272"},
+            {"All Cities", "23424982"},
+            {"Barquisimeto", "468382"}
+    };
+
+    public static String[][] getArray(String countryName) {
+        if (countryName.equals("Argentina")) {
+            return argentina;
+        } else if (countryName.equals("Australia")) {
+            return australia;
+        } else if (countryName.equals("Brazil")) {
+            return brazil;
+        } else if (countryName.equals("Canada")) {
+            return canada;
+        } else if (countryName.equals("Chile")) {
+            return chile;
+        } else if (countryName.equals("Colombia")) {
+            return colombia;
+        } else if (countryName.equals("Dominican Republic")) {
+            return dominican_republic;
+        } else if (countryName.equals("Ecuador")) {
+            return ecuador;
+        } else if (countryName.equals("France")) {
+            return france;
+        } else if (countryName.equals("Germany")) {
+            return germany;
+        } else if (countryName.equals("Guatemala")) {
+            return guatemala;
+        } else if (countryName.equals("India")) {
+            return india;
+        } else if (countryName.equals("Indonesia")) {
+            return indonesia;
+        } else if (countryName.equals("Ireland")) {
+            return ireland;
+        } else if (countryName.equals("Italy")) {
+            return italy;
+        } else if (countryName.equals("Japan")) {
+            return japan;
+        } else if (countryName.equals("Malaysia")) {
+            return malaysia;
+        } else if (countryName.equals("Mexico")) {
+            return mexico;
+        } else if (countryName.equals("Netherlands")) {
+            return netherlands;
+        } else if (countryName.equals("New Zealand")) {
+            return new_zealand;
+        } else if (countryName.equals("Nigeria")) {
+            return nigeria;
+        } else if (countryName.equals("Pakistan")) {
+            return pakistan;
+        } else if (countryName.equals("Peru")) {
+            return peru;
+        } else if (countryName.equals("Philippines")) {
+            return philippines;
+        } else if (countryName.equals("Russia")) {
+            return russia;
+        } else if (countryName.equals("Singapore")) {
+            return singapore;
+        } else if (countryName.equals("South Africa")) {
+            return south_africa;
+        } else if (countryName.equals("Spain")) {
+            return spain;
+        } else if (countryName.equals("Sweden")) {
+            return sweden;
+        } else if (countryName.equals("Turkey")) {
+            return turkey;
+        } else if (countryName.equals("United Arab Emirates")) {
+            return united_arab_emirates;
+        } else if (countryName.equals("United Kingdom")) {
+            return united_kingdom;
+        } else if (countryName.equals("United States")) {
+            return united_states;
+        } else if (countryName.equals("Venezuela")) {
+            return venezuela;
+        }
+
+        return null;
+    }
+}
