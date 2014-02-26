7dfee74000be3be02f7d03a9eda167e3fdd2331d
lukeklinker <lklinker1@gmail.com>
2014-02-25 21:35:34 -0600

    quick fix

--- src/com/klinker/android/twitter/utils/text/TextUtils.java
+++ src/com/klinker/android/twitter/utils/text/TextUtils.java
@@ -59,7 +59,7 @@ public class TextUtils {
             Log.v("talon_text", "Found: " + m.group(0));
             finish = changeText(finish, m.group(0), color);
         }
-        m = Regex.WEB_URL_PATTERN.matcher(tweet);
+        m = Patterns.WEB_URL.matcher(tweet);
         while (m.find()) {
             Log.v("talon_text", "Found: " + m.group(0));
             finish = changeText(finish, m.group(0), color);
