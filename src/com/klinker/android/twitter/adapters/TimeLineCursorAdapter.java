b94b8f6b4180cb85bcd8eeed8e2431c8fa36ff3d
Luke Klinker <lkinker1@gmail.com>
2014-03-21 22:19:16 -0500

    put the click listener on the retweeter too

--- src/com/klinker/android/twitter/adapters/TimeLineCursorAdapter.java
+++ src/com/klinker/android/twitter/adapters/TimeLineCursorAdapter.java
@@ -796,7 +796,33 @@ public class TimeLineCursorAdapter extends CursorAdapter {
                         }
                     });
 
+                    if (holder.retweeter.getVisibility() == View.VISIBLE) {
+                        holder.retweeter.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                if (!holder.preventNextClick) {
+                                    holder.background.getBackground().setState(new int[]{android.R.attr.state_pressed});
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            holder.background.getBackground().setState(new int[]{android.R.attr.state_empty});
+                                        }
+                                    }, 25);
+                                }
 
+                                holder.background.performClick();
+                            }
+                        });
+
+                        holder.retweeter.setOnLongClickListener(new View.OnLongClickListener() {
+                            @Override
+                            public boolean onLongClick(View view) {
+                                holder.background.performLongClick();
+                                holder.preventNextClick = true;
+                                return false;
+                            }
+                        });
+                    }
 
                 }
             }
