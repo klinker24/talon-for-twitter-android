c6db04992f51eaa3acd7c5e72f180b7466bddc5a
lukeklinker <lklinker1@gmail.com>
2014-04-23 12:07:35 -0500

    one shared prefs thing

--- src/com/klinker/android/twitter/services/StopPull.java
+++ src/com/klinker/android/twitter/services/StopPull.java
@@ -16,10 +16,12 @@ public class StopPull extends IntentService {
 
     @Override
     public void onHandleIntent(Intent intent) {
-
         SharedPreferences sharedPreferences = getSharedPreferences("com.klinker.android.twitter_world_preferences",
                 Context.MODE_WORLD_READABLE + Context.MODE_WORLD_WRITEABLE);
         sharedPreferences.edit().putBoolean("push_notifications", false).commit();
+
+        // write to normal prefs so that it appears in the settings
+        PreferenceManager.getDefaultSharedPreferences(this).edit().putBoolean("push_notifications", false).commit();
 
         sendBroadcast(new Intent("com.klinker.android.twitter.STOP_PUSH_SERVICE"));
     }
