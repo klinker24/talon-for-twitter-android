0f18d7550be8697c711d694fbee0fece751438f5
lukeklinker <lklinker1@gmail.com>
2014-04-29 09:16:34 -0500

    dont show the drawer on the popup from the launcher because it doesn't work correctly lol

--- src/com/klinker/android/twitter/widget/launcher_fragment/LauncherPopup.java
+++ src/com/klinker/android/twitter/widget/launcher_fragment/LauncherPopup.java
@@ -1,8 +1,11 @@
 package com.klinker.android.twitter.widget.launcher_fragment;
 
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
+import com.klinker.android.twitter.R;
 import com.klinker.android.twitter.ui.MainActivityPopup;
 
 
@@ -10,7 +13,6 @@ public class LauncherPopup extends MainActivityPopup {
 
     @Override
     public void setDim() {
-        Log.v("talon_launcher", "setting dim");
         // Params for the window.
         // You can easily set the alpha and the dim behind the window from here
         WindowManager.LayoutParams params = getWindow().getAttributes();
@@ -23,5 +25,7 @@ public class LauncherPopup extends MainActivityPopup {
     @Override
     public void setLauncherPage() {
         mViewPager.setCurrentItem(getIntent().getIntExtra("launcher_page", 0));
+        LinearLayout drawer = (LinearLayout) findViewById(R.id.left_drawer);
+        drawer.setVisibility(View.GONE);
     }
 }
