32f1749c6ed5762c465018a20eab3bfd3a7643a6
Luke Klinker <lkinker1@gmail.com>
2014-05-10 15:53:34 -0500

    try loading the launcher page class from the application class... someone on the internet said it would help lol we will see I guess. Still don't have clue what is going wrong

--- src/com/klinker/android/twitter/data/App.java
+++ src/com/klinker/android/twitter/data/App.java
@@ -3,6 +3,7 @@ package com.klinker.android.twitter.data;
 import android.app.Application;
 import android.content.Context;
 
+import com.klinker.android.twitter.ui.launcher_page.LauncherPage;
 import com.klinker.android.twitter.utils.EmojiUtils;
 
 import java.io.File;
@@ -11,6 +12,12 @@ import uk.co.senab.bitmapcache.BitmapLruCache;
 
 public class App extends Application {
     private BitmapLruCache mCache;
+
+    public App() {
+        super();
+        ClassLoader mClassLoader = LauncherPage.class.getClassLoader();
+        Thread.currentThread().setContextClassLoader(mClassLoader);
+    }
 
     @Override
     public void onCreate() {
