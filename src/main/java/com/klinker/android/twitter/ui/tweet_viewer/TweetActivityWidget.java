af4882a5ccfed2c554aeea2d8f26b8d07c1c6e0e
lukeklinker <lklinker1@gmail.com>
2014-06-12 09:47:01 -0500

    changes to attaching pictures from the widget tweet viewer... just opens the regular one and does it there
    Former-commit-id: 65821ac3a5b9221d01aa1bd1db6dc8958f918fdb

--- src/main/java/com/klinker/android/twitter/ui/tweet_viewer/TweetActivityWidget.java
+++ src/main/java/com/klinker/android/twitter/ui/tweet_viewer/TweetActivityWidget.java
@@ -1,5 +1,31 @@
 package com.klinker.android.twitter.ui.tweet_viewer;
 
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
 public class TweetActivityWidget extends TweetPager {
+
+    BroadcastReceiver attach = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            startActivity(new Intent(context, TweetPager.class).putExtras(getIntent()));
+            finish();
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(attach, new IntentFilter("com.klinker.android.twitter.ATTACH_BUTTON"));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(attach);
+    }
 
 }
