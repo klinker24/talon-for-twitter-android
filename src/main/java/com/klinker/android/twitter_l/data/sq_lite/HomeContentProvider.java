624c85de098f81a9a6dc9f07848324cb2087b022
lukeklinker <lklinker1@gmail.com>
2014-07-01 07:59:39 -0500

    few more fixes
    Former-commit-id: d3be934399361a7452fc2a5c72190c9fd1ca9768

--- src/main/java/com/klinker/android/twitter_l/data/sq_lite/HomeContentProvider.java
+++ src/main/java/com/klinker/android/twitter_l/data/sq_lite/HomeContentProvider.java
@@ -288,7 +288,7 @@ public class HomeContentProvider extends ContentProvider {
         values.put(HomeSQLiteHelper.COLUMN_TEXT, text);
         values.put(HomeSQLiteHelper.COLUMN_TWEET_ID, id);
         values.put(HomeSQLiteHelper.COLUMN_NAME, status.getUser().getName());
-        values.put(HomeSQLiteHelper.COLUMN_PRO_PIC, status.getUser().getBiggerProfileImageURL());
+        values.put(HomeSQLiteHelper.COLUMN_PRO_PIC, status.getUser().getOriginalProfileImageURL());
         values.put(HomeSQLiteHelper.COLUMN_SCREEN_NAME, status.getUser().getScreenName());
         values.put(HomeSQLiteHelper.COLUMN_TIME, time);
         values.put(HomeSQLiteHelper.COLUMN_RETWEETER, originalName);
@@ -338,7 +338,7 @@ public class HomeContentProvider extends ContentProvider {
             values.put(HomeSQLiteHelper.COLUMN_TEXT, text);
             values.put(HomeSQLiteHelper.COLUMN_TWEET_ID, mId);
             values.put(HomeSQLiteHelper.COLUMN_NAME, status.getUser().getName());
-            values.put(HomeSQLiteHelper.COLUMN_PRO_PIC, status.getUser().getBiggerProfileImageURL());
+            values.put(HomeSQLiteHelper.COLUMN_PRO_PIC, status.getUser().getOriginalProfileImageURL());
             values.put(HomeSQLiteHelper.COLUMN_SCREEN_NAME, status.getUser().getScreenName());
             values.put(HomeSQLiteHelper.COLUMN_TIME, time);
             values.put(HomeSQLiteHelper.COLUMN_RETWEETER, originalName);
