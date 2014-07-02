7e844f26b9fb5dbcd47b152bcc41f13bc8889f87
Luke Klinker <lkinker1@gmail.com>
2014-07-02 12:38:27 -0500

    few small style things
    Former-commit-id: 9a67c2807095a294d9c75bd5fdfd1cf68fd13e7c

--- src/main/java/com/klinker/android/twitter_l/ui/tweet_viewer/TweetPager.java
+++ src/main/java/com/klinker/android/twitter_l/ui/tweet_viewer/TweetPager.java
@@ -1372,25 +1372,7 @@ public class TweetPager extends YouTubeBaseActivity implements
     }
 
     public void adjustConversationSectionSize(AsyncListView listView) {
-        /*ListAdapter listAdapter = listView.getAdapter();
-        if (listAdapter == null)
-            return;
-
-        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
-        int totalHeight = 0;
-        View view = null;
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            view = listAdapter.getView(i, view, listView);
-            if (i == 0)
-                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-            totalHeight += view.getMeasuredHeight();
-        }
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1)) + Utils.toDP(200, context);
-        listView.setLayoutParams(params);
-        listView.requestLayout();*/
+        findViewById(R.id.nav_bar_seperator).setVisibility(View.GONE);
     }
 
     public boolean isRunning = true;
