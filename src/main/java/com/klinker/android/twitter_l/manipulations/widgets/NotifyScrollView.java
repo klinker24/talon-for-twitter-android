e2adeeb0c90bb4484404cbb81e55f925d63cbbdd
Luke Klinker <lkinker1@gmail.com>
2014-07-02 12:26:51 -0500

    youtube and conversation sections on the tweet viewer
    Former-commit-id: b0e8d5a95495f4c0c24e69187c445141bfb9f234

--- src/main/java/com/klinker/android/twitter_l/manipulations/widgets/NotifyScrollView.java
+++ src/main/java/com/klinker/android/twitter_l/manipulations/widgets/NotifyScrollView.java
@@ -2,6 +2,8 @@ package com.klinker.android.twitter_l.manipulations.widgets;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.ScrollView;
 
 /**
@@ -28,6 +30,41 @@ public class NotifyScrollView extends ScrollView {
 
     public NotifyScrollView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = ev.getAction();
+        switch (action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                Log.i("VerticalScrollview", "onInterceptTouchEvent: DOWN super false" );
+                super.onTouchEvent(ev);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                return false; // redirect MotionEvents to ourself
+
+            case MotionEvent.ACTION_CANCEL:
+                Log.i("VerticalScrollview", "onInterceptTouchEvent: CANCEL super false" );
+                super.onTouchEvent(ev);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                Log.i("VerticalScrollview", "onInterceptTouchEvent: UP super false" );
+                return false;
+
+            default: Log.i("VerticalScrollview", "onInterceptTouchEvent: " + action ); break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        super.onTouchEvent(ev);
+        Log.i("VerticalScrollview", "onTouchEvent. action: " + ev.getAction());
+        return true;
     }
 
     @Override
