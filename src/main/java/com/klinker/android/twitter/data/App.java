f743a89317a610d8afa0b74ca43c14ce957a0095
lukeklinker <lklinker1@gmail.com>
2014-07-29 07:19:48 -0500

    some caching fixes
    Former-commit-id: 8190b9b8d1bc8a881efa0121da89498fde9d8ad6

--- src/main/java/com/klinker/android/twitter/data/App.java
+++ src/main/java/com/klinker/android/twitter/data/App.java
@@ -11,48 +11,45 @@ import uk.co.senab.bitmapcache.BitmapLruCache;
 
 public class App extends Application {
     private BitmapLruCache mCache;
+    private BitmapLruCache profileCache;
+
+    private static final int MEGA_BYTE = 1024 * 1024;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        try {
-            File cacheDir = new File(createPackageContext("com.klinker.android.twitter", CONTEXT_IGNORE_SECURITY).getCacheDir(), "talon");
-            cacheDir.mkdirs();
+        File cacheDir = new File(getCacheDir(), "talon");
+        cacheDir.mkdirs();
 
-            BitmapLruCache.Builder builder = new BitmapLruCache.Builder();
-            builder.setMemoryCacheEnabled(true).setMemoryCacheMaxSizeUsingHeapSize();
-            builder.setDiskCacheEnabled(true).setDiskCacheLocation(cacheDir);
+        File proCacheDir = new File(getCacheDir(), "talon-profile");
+        proCacheDir.mkdirs();
 
-            mCache = builder.build();
+        BitmapLruCache.Builder builder = new BitmapLruCache.Builder();
+        builder.setMemoryCacheEnabled(true).setMemoryCacheMaxSizeUsingHeapSize(.25f);
+        builder.setDiskCacheEnabled(true).setDiskCacheLocation(cacheDir).setDiskCacheMaxSize(100 * MEGA_BYTE);
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    EmojiUtils.init(App.this);
-                }
-            }).start();
-        } catch (Exception e) {
-            File cacheDir = new File(getCacheDir(), "talon");
-            cacheDir.mkdirs();
+        mCache = builder.build();
 
-            BitmapLruCache.Builder builder = new BitmapLruCache.Builder();
-            builder.setMemoryCacheEnabled(true).setMemoryCacheMaxSizeUsingHeapSize();
-            builder.setDiskCacheEnabled(true).setDiskCacheLocation(cacheDir);
+        builder.setDiskCacheLocation(proCacheDir);
 
-            mCache = builder.build();
+        profileCache = builder.build();
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    EmojiUtils.init(App.this);
-                }
-            }).start();
-        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                EmojiUtils.init(App.this);
+            }
+        }).start();
+
     }
 
     public BitmapLruCache getBitmapCache() {
         return mCache;
+    }
+
+    public BitmapLruCache getProfileCache() {
+        return profileCache;
     }
 
     public static App getInstance(Context context) {
