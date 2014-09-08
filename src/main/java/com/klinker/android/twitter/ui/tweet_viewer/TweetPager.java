dd4e85d884813706265984129814a6f97eefa96c
Luke Klinker <lklinker1@gmail.com>
2014-09-08 15:18:46 -0500

    version 3.1.2
    Former-commit-id: c263c29006f0fd6225ef58e001141c9a39ba7c9f

--- src/main/java/com/klinker/android/twitter/ui/tweet_viewer/TweetPager.java
+++ src/main/java/com/klinker/android/twitter/ui/tweet_viewer/TweetPager.java
@@ -836,9 +836,13 @@ public class TweetPager extends YouTubeBaseActivity {
     }
 
     private static String stripTrailingPeriods(String url) {
-        if (url.substring(url.length() - 1, url.length()).equals(".")) {
-            return stripTrailingPeriods(url.substring(0, url.length() - 1));
-        } else {
+        try {
+            if (url.substring(url.length() - 1, url.length()).equals(".")) {
+                return stripTrailingPeriods(url.substring(0, url.length() - 1));
+            } else {
+                return url;
+            }
+        } catch (Exception e) {
             return url;
         }
     }
