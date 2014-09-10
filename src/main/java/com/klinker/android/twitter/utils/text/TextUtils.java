9e71e19085e8246d29182c06cba966fa09c76a29
Luke Klinker <lklinker1@gmail.com>
2014-09-10 15:08:16 -0500

    much better hashtag, mention, and url regex matching using some of twitter's own regex strings
    Former-commit-id: fb458347d0ee2247a2d323e9e5c8d3799181bed7

--- src/main/java/com/klinker/android/twitter/utils/text/TextUtils.java
+++ src/main/java/com/klinker/android/twitter/utils/text/TextUtils.java
@@ -45,7 +45,7 @@ public class TextUtils {
 
         //Linkify.addLinks(context, textView, Patterns.PHONE, null, filter, textView, holder);
         Linkify.addLinks(context, textView, Patterns.EMAIL_ADDRESS, null, filter, textView, holder, allLinks, extBrowser);
-        Linkify.addLinks(context, textView, Patterns.WEB_URL, null, filter, textView, holder, allLinks, extBrowser);
+        Linkify.addLinks(context, textView, Regex.VALID_URL, null, filter, textView, holder, allLinks, extBrowser);
         Linkify.addLinks(context, textView, Regex.HASHTAG_PATTERN, null, filter, textView, holder, allLinks, extBrowser);
         Linkify.addLinks(context, textView, Regex.CASHTAG_PATTERN, null, filter, textView, holder, allLinks, extBrowser);
         Linkify.addLinks(context, textView, Regex.MENTION_PATTERN, null, filter, textView, holder, allLinks, extBrowser);
@@ -62,7 +62,7 @@ public class TextUtils {
 
         //Linkify.addLinks(context, textView, Patterns.PHONE, null, filter, textView, holder);
         Linkify.addLinks(context, settings, textView, Patterns.EMAIL_ADDRESS, null, filter, textView, holder, allLinks, extBrowser);
-        Linkify.addLinks(context, settings, textView, Patterns.WEB_URL, null, filter, textView, holder, allLinks, extBrowser);
+        Linkify.addLinks(context, settings, textView, Regex.VALID_URL, null, filter, textView, holder, allLinks, extBrowser);
         Linkify.addLinks(context, settings, textView, Regex.HASHTAG_PATTERN, null, filter, textView, holder, allLinks, extBrowser);
         Linkify.addLinks(context, settings, textView, Regex.CASHTAG_PATTERN, null, filter, textView, holder, allLinks, extBrowser);
         Linkify.addLinks(context, settings, textView, Regex.MENTION_PATTERN, null, filter, textView, holder, allLinks, extBrowser);
@@ -87,7 +87,7 @@ public class TextUtils {
         while (m.find()) {
             finish = changeText(finish, m.group(0), color);
         }
-        m = Patterns.WEB_URL.matcher(tweet);
+        m = Regex.VALID_URL.matcher(tweet);
         while (m.find()) {
             finish = changeText(finish, m.group(0), color);
         }
