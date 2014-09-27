3bf75e682a998659727ec831793535457f0213d5
Luke Klinker <lklinker1@gmail.com>
2014-09-26 22:00:06 -0500

    finished with the login activity
    Former-commit-id: 832d1798a83128987736cd9d1c96ba21aff07d89

--- src/main/java/com/klinker/android/twitter_l/manipulations/FollowMePopup.java
+++ src/main/java/com/klinker/android/twitter_l/manipulations/FollowMePopup.java
@@ -2,9 +2,20 @@ package com.klinker.android.twitter_l.manipulations;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.view.View;
+import android.widget.LinearLayout;
 
+import com.klinker.android.twitter_l.R;
+import com.klinker.android.twitter_l.data.App;
+import com.klinker.android.twitter_l.manipulations.widgets.NetworkedCacheableImageView;
 import com.klinker.android.twitter_l.manipulations.widgets.PopupLayout;
+import com.klinker.android.twitter_l.settings.AppSettings;
+import com.klinker.android.twitter_l.utils.Utils;
+
+import twitter4j.Twitter;
 
 /**
  * Created by luke on 9/26/14.
@@ -15,12 +26,77 @@ public class FollowMePopup extends PopupLayout {
     public FollowMePopup(Context context) {
         super(context);
         this.context = context;
+
+        setTitle(getResources().getString(R.string.follow_progress));
     }
 
     @Override
     public View setMainLayout() {
-        View root = ((Activity)context).getLayoutInflater().inflate(R.layout.follow_me_popup, null, false);
+        View root = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.follow_me_popup, null, false);
+
+        LinearLayout talon = (LinearLayout) root.findViewById(R.id.talon_area);
+        NetworkedCacheableImageView talonIv = (NetworkedCacheableImageView) root.findViewById(R.id.talon_picture);
+
+        LinearLayout luke = (LinearLayout) root.findViewById(R.id.luke_area);
+        NetworkedCacheableImageView lukeIv = (NetworkedCacheableImageView) root.findViewById(R.id.luke_picture);
+
+        LinearLayout google = (LinearLayout) root.findViewById(R.id.google_plus_area);
+        NetworkedCacheableImageView googleIv = (NetworkedCacheableImageView) root.findViewById(R.id.google_picture);
+
+        talonIv.loadImage("https://pbs.twimg.com/profile_images/496279971094986753/9NVnIz-m.png", true, null);
+        lukeIv.loadImage("https://pbs.twimg.com/profile_images/497466110892331009/_iR38HDB.jpeg", true, null);
+        googleIv.loadImage("https://developers.google.com/+/images/branding/g+128.png", true, null);
+
+        talonIv.setClipToOutline(true);
+        lukeIv.setClipToOutline(true);
+
+        talon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FollowMe().execute("TalonAndroid");
+
+                view.setAlpha(.4f);
+                view.setEnabled(false);
+            }
+        });
+
+        luke.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FollowMe().execute("lukeklinker");
+
+                view.setAlpha(.4f);
+                view.setEnabled(false);
+            }
+        });
+
+        google.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com/+LukeKlinker")));
+
+                view.setAlpha(.4f);
+                view.setEnabled(false);
+            }
+        });
 
         return root;
+    }
+
+    class FollowMe extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... urls) {
+            Twitter twit = Utils.getTwitter(getContext(), AppSettings.getInstance(getContext()));
+
+            try {
+                twit.createFriendship(urls[0]);
+            } catch (Exception x) {
+                x.printStackTrace();
+            }
+
+            return null;
+        }
+
     }
 }
