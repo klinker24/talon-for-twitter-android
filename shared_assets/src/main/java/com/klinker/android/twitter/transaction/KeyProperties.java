b02dce8d152af69c9fa66c93806a5c1f9f8d2545
Luke Klinker <lklinker1@gmail.com>
2014-09-29 14:53:00 -0500

    well... the wear app installs haha
    Former-commit-id: 584946de510cae143722b3cdc7bae1071010ed33

---
+++ shared_assets/src/main/java/com/klinker/android/twitter/transaction/KeyProperties.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 Jacob Klinker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klinker.android.twitter.transaction;
+
+public class KeyProperties {
+
+    public static final String PATH = "/source_articles";
+    public static final String PATH_NOTIFICATION = "/source_notification";
+    public static final String GET_DATA_MESSAGE = "get_data_for_wearable";
+    public static final String MARK_READ_MESSAGE = "mark_article_read";
+    public static final String REQUEST_IMAGE = "request_image";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_BODY = "body";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_ID = "id";
+    public static final String KEY_PRIMARY_COLOR = "primary_color";
+    public static final String KEY_ACCENT_COLOR = "accent_color";
+    public static final String KEY_IMAGE_NAME = "image_name";
+    public static final String KEY_IMAGE_DATA = "image_data";
+    public static final String LINE_BREAK = "__INSERT_BREAK_HERE__";
+    public static final String DIVIDER = "__~~~~__";
+
+}
