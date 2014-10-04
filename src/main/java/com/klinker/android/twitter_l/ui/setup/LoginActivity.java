c0196a079b6f156a9425cfbd24f982cb99f054a9
Luke Klinker <lklinker1@gmail.com>
2014-10-04 15:24:23 -0500

    fixed the issues with login
    Former-commit-id: e3409e222f1e48a96d52bf57c4901b398d17d3ee

--- src/main/java/com/klinker/android/twitter_l/ui/setup/LoginActivity.java
+++ src/main/java/com/klinker/android/twitter_l/ui/setup/LoginActivity.java
@@ -22,6 +22,8 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -186,6 +188,10 @@ public class LoginActivity extends Activity {
         title.setText(getResources().getString(R.string.first_welcome));
         summary.setText(getResources().getString(R.string.first_info));
 
+        CookieSyncManager.createInstance(this);
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookie();
+
         mWebView = (WebView)findViewById(R.id.loginWebView);
         try {
             mWebView.getSettings().setJavaScriptEnabled(true);
@@ -236,20 +242,6 @@ public class LoginActivity extends Activity {
                 if (btnLoginTwitter.getText().toString().contains(getResources().getString(R.string.login_to_twitter))) {
                     if (Utils.hasInternetConnection(context)) {
                         btnLoginTwitter.setEnabled(false);
-
-                        if (sharedPrefs.getInt("current_account", 1) == 2) {
-                            new AlertDialog.Builder(context)
-                                    .setTitle(getString(R.string.second_account))
-                                    .setMessage(getString(R.string.second_account_login_info))
-                                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                            dialogInterface.dismiss();
-                                        }
-                                    })
-                                    .create()
-                                    .show();
-                        }
 
                         new RetreiveFeedTask().execute();
                     } else {
