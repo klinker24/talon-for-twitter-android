76067490aa4c6435c6953ffef988f6814e5af457
Luke Klinker <lklinker1@gmail.com>
2014-10-10 15:11:57 -0500

    tried more with the following status, but it isn't working and I can't figure out why yet
    Former-commit-id: 699df0d7dd16803219120cc0a237b5ebe87b8abc

--- src/main/java/com/klinker/android/twitter_l/ui/profile_viewer/fragments/ProfileFragment.java
+++ src/main/java/com/klinker/android/twitter_l/ui/profile_viewer/fragments/ProfileFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -555,7 +556,7 @@ public class ProfileFragment extends Fragment {
         getData.start();
     }
 
-    public PeopleArrayAdapter followersAdapter;
+    public ArrayAdapter<User> followersAdapter;
 
     public void getFollowers(final User user, final AsyncListView listView) {
         spinner.setVisibility(View.VISIBLE);
