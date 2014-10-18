2387f463818bf8cd91a559c2d693c22924741ba6
Luke Klinker <lklinker1@gmail.com>
2014-10-18 09:34:16 -0500

    removed twitpic again
    Former-commit-id: 5f75fe1c8bf625276df1b52db9509177a9359c4f

--- src/main/java/com/klinker/android/twitter/ui/compose/ComposeDMActivity.java
+++ src/main/java/com/klinker/android/twitter/ui/compose/ComposeDMActivity.java
@@ -153,6 +153,7 @@ public class ComposeDMActivity extends Compose {
             }
         }
 
+        attachButton.setVisibility(View.GONE);
         attachButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
