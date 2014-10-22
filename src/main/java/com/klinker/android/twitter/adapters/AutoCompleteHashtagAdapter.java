72d79e92149b8ccf8ed3faee109d70939da26b72
Luke Klinker <lklinker1@gmail.com>
2014-10-22 10:07:05 -0500

    wow. licenses
    Former-commit-id: c4a74fb8dd11928ed8283a53cce2b8fa990e05b0

--- src/main/java/com/klinker/android/twitter/adapters/AutoCompleteHashtagAdapter.java
+++ src/main/java/com/klinker/android/twitter/adapters/AutoCompleteHashtagAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Luke Klinker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.klinker.android.twitter.adapters;
 
 import android.app.Activity;
@@ -21,9 +37,6 @@ import com.klinker.android.twitter.utils.AutoCompleteHelper;
 
 import java.util.ArrayList;
 
-/**
- * Created by luke on 6/19/14.
- */
 public class AutoCompleteHashtagAdapter extends CursorAdapter {
 
     private Cursor cursor;
