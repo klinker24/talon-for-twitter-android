27b09c90b3c1c11ffbe1dbcbe66f4d287e9bcbb1
Luke Klinker <lklinker1@gmail.com>
2014-10-22 08:57:32 -0500

    commented the API key class
    Former-commit-id: e8089fc082cdd3515a76b0beb823307b77ec9703

--- src/main/java/com/klinker/android/twitter/APIKeys.java
+++ src/main/java/com/klinker/android/twitter/APIKeys.java
@@ -2,13 +2,41 @@ package com.klinker.android.twitter;
 
 public class APIKeys {
 
-    // Twitter's API Keys
-    // THESE TWO ARE THE ONLY ONES REQUIRED TO RUN THE APP
+    /**
+     * Twitter's API Keys
+     * THESE TWO ARE THE ONLY ONES REQUIRED TO RUN THE APP!
+     * They are easy to obtain from Twitter
+     *
+     * Sign in with your Twitter credentials here:
+     * https://apps.twitter.com/
+     *
+     * Then create a new application.
+     *
+     * For steps to creating an application, view the Readme.md
+     */
     public static String TWITTER_CONSUMER_KEY = "V9yijGrKf79jlYi0l3ekpA";
     public static String TWITTER_CONSUMER_SECRET = "IHHoYqukYC951gsP8gkhr1RUSBJYYwhGO0P3uuCDkA";
 
+    /**
+     * For the In-App Youtube Player
+     * It WILL NOT work if you do not obtain a key for yourself.
+     * It is easy to get one of these though.
+     *
+     * Here is how you can get a key for yourself:
+     * https://developers.google.com/youtube/android/player/register
+     */
     public static String YOUTUBE_API_KEY = "AIzaSyCCL7Rem3uo1zPBpy88KANXIaX2_bYWEtM";
 
+    /**
+     * These are third party service API keys for Talon.
+     *
+     * If you wish to use these services, You will need to get a key as I will not be sharing mine
+     * for obvious security reasons.
+     *
+     * Tweetmarker is a paid service, so if you want a key, you will have to pay $75 a month for it
+     * For Twitlonger, you must request access to their API for your app. I do not know if he would grant an Open Source Api key or not.
+     * TwitPic is dead, but I kept its classes in here so that you can still learn from them. The service no longer is supported.
+     */
     public static final String TWEETMARKER_API_KEY = "***REMOVED***";
     public static final String TWITLONGER_API_KEY = "***REMOVED***";
     public static final String TWITPIC_API_KEY = "8cd3757bb6acb94c61e3cbf840c91872";
