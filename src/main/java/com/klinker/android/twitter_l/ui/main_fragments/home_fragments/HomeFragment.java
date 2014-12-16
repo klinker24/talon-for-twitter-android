d8b941713c5317b3aa882d1757504edc71f48bfa
Luke Klinker <lklinker1@gmail.com>
2014-12-16 10:14:49 -0600

    bug fixes
    Former-commit-id: 82a5cf1a05f5d1abb345c1805d461a86f8b14555

--- src/main/java/com/klinker/android/twitter_l/ui/main_fragments/home_fragments/HomeFragment.java
+++ src/main/java/com/klinker/android/twitter_l/ui/main_fragments/home_fragments/HomeFragment.java
@@ -999,32 +999,29 @@ public class HomeFragment extends MainFragment { // implements LoaderManager.Loa
             }, 600);
         }
 
-        if (!sharedPrefs.getBoolean("from_activity", false)) {
-            if (settings.liveStreaming && settings.tweetmarker && !settings.tweetmarkerManualOnly) {
-                HomeFragment.refreshHandler.removeCallbacksAndMessages(null);
-                HomeFragment.refreshHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        fetchTweetMarker();
-                    }
-                }, 600);
-            } else if (!settings.liveStreaming && settings.tweetmarker && !settings.tweetmarkerManualOnly) {
-                HomeFragment.refreshHandler.removeCallbacksAndMessages(null);
-                HomeFragment.refreshHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (!actionBar.isShowing() && !isLauncher()) {
-                            showStatusBar();
-                            actionBar.show();
-                        }
 
-                        refreshOnStart();
+        if (settings.liveStreaming && settings.tweetmarker && !settings.tweetmarkerManualOnly) {
+            HomeFragment.refreshHandler.removeCallbacksAndMessages(null);
+            HomeFragment.refreshHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    fetchTweetMarker();
+                }
+            }, 600);
+        } else if (!settings.liveStreaming && settings.tweetmarker && !settings.tweetmarkerManualOnly) {
+            HomeFragment.refreshHandler.removeCallbacksAndMessages(null);
+            HomeFragment.refreshHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (!actionBar.isShowing() && !isLauncher()) {
+                        showStatusBar();
+                        actionBar.show();
                     }
-                }, 600);
-            }
+
+                    refreshOnStart();
+                }
+            }, 600);
         }
-
-        sharedPrefs.edit().putBoolean("from_activity", false).commit();
 
         context.sendBroadcast(new Intent("com.klinker.android.twitter.CLEAR_PULL_UNREAD"));
     }
