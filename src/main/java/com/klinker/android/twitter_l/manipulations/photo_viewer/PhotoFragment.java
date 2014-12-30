32347e175510c0149486363c5f389737ddc98183
Luke Klinker <lklinker1@gmail.com>
2014-12-30 17:48:12 -0500

    fix a force close
    Former-commit-id: 19b22b7e5964e74405749246c4a6cf98ab84be27

--- src/main/java/com/klinker/android/twitter_l/manipulations/photo_viewer/PhotoFragment.java
+++ src/main/java/com/klinker/android/twitter_l/manipulations/photo_viewer/PhotoFragment.java
@@ -263,18 +263,26 @@ public class PhotoFragment extends Fragment {
     private void hideSystemUI() {
         sysUiShown = false;
 
-        getActivity().getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        try {
+            getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        } catch (Exception e) {
+            // fragment not attached to activity
+        }
     }
 
     boolean sysUiShown = true;
     private void showSystemUI() {
         sysUiShown = true;
 
-        getActivity().getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        try {
+            getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        } catch (Exception e) {
+            // fragment not attached to activity
+        }
     }
 }
