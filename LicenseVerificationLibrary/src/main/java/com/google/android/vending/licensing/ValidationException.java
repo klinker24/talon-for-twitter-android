406325b18f461010a017d3106a23b621a48d838f
Luke Klinker <lklinker1@gmail.com>
2015-01-01 11:55:52 -0500

    added license and certificate signature check to login activity
    Former-commit-id: b0be7fc693e59e7b067ce0f4e8fe03fcad0f4130

--- LicenseVerificationLibrary/src/main/java/com/google/android/vending/licensing/ValidationException.java
+++ LicenseVerificationLibrary/src/main/java/com/google/android/vending/licensing/ValidationException.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2014 Jacob Klinker
+ * Copyright (C) 2010 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,4 +14,20 @@
  * limitations under the License.
  */
 
-include ':wear', ':shared_assets', ':LicenseVerificationLibrary'
+package com.google.android.vending.licensing;
+
+/**
+ * Indicates that an error occurred while validating the integrity of data managed by an
+ * {@link Obfuscator}.}
+ */
+public class ValidationException extends Exception {
+    public ValidationException() {
+      super();
+    }
+
+    public ValidationException(String s) {
+      super(s);
+    }
+
+    private static final long serialVersionUID = 1L;
+}
