ae84861a7e315c1ff743a3904c837487237eb9f7
Luke Klinker <lklinker1@gmail.com>
2015-07-23 09:21:43 -0500

    fix issue with first install not showing any content because the pages were never set up
    Former-commit-id: a588a640665c0508bb9431cbabd58ee7ce09181a

--- src/main/java/com/klinker/android/twitter_l/utils/UpdateUtils.java
+++ src/main/java/com/klinker/android/twitter_l/utils/UpdateUtils.java
@@ -72,7 +72,52 @@ public class UpdateUtils {
             sharedPrefs.edit().putLong("rate_it_last_shown", currentTime).commit();
         }
 
+        runFirstInstalled(context, sharedPrefs);
         runEveryUpdate(context, sharedPrefs);
+    }
+
+    public static void runFirstInstalled(final Context context, final SharedPreferences sharedPrefs) {
+        if (sharedPrefs.getBoolean("fresh_install", true)) {
+            SharedPreferences.Editor e = sharedPrefs.edit();
+            e.putBoolean("fresh_install", false);
+
+            // show them all for now
+            Set<String> set = new HashSet<String>();
+            set.add("0"); // activity
+            set.add("1"); // timeline
+            set.add("2"); // mentions
+            set.add("3"); // dm's
+            set.add("4"); // discover
+            set.add("5"); // lists
+            set.add("6"); // favorite users
+            set.add("7"); // retweets
+            set.add("8"); // favorite Tweets
+            set.add("9"); // saved searches
+
+            e.putStringSet("drawer_elements_shown_1", set);
+            e.putStringSet("drawer_elements_shown_2", set);
+
+            // reset their pages to just home,
+            String pageIdentifier = "account_" + 1 + "_page_";
+            e.putInt(pageIdentifier + 1, AppSettings.PAGE_TYPE_ACTIVITY);
+            e.putInt(pageIdentifier + 2, AppSettings.PAGE_TYPE_HOME);
+            e.putInt(pageIdentifier + 3, AppSettings.PAGE_TYPE_MENTIONS);
+            e.putInt(pageIdentifier + 4, AppSettings.PAGE_TYPE_DMS);
+
+            pageIdentifier = "account_" + 2 + "_page_";
+            e.putInt(pageIdentifier + 1, AppSettings.PAGE_TYPE_ACTIVITY);
+            e.putInt(pageIdentifier + 2, AppSettings.PAGE_TYPE_HOME);
+            e.putInt(pageIdentifier + 3, AppSettings.PAGE_TYPE_MENTIONS);
+            e.putInt(pageIdentifier + 4, AppSettings.PAGE_TYPE_DMS);
+
+            e.putInt("default_timeline_page_" + 1, 1);
+            e.putInt("default_timeline_page_" + 2, 1);
+
+            sharedPrefs.edit().putLong("original_activity_refresh_" + 1, Calendar.getInstance().getTimeInMillis()).commit();
+            sharedPrefs.edit().putLong("original_activity_refresh_" + 2, Calendar.getInstance().getTimeInMillis()).commit();
+
+            e.commit();
+        }
     }
 
     public static void runEveryUpdate(final Context context, final SharedPreferences sharedPrefs) {
