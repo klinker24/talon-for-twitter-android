6cd88fc0ffb309ddc87af04f70f83c0ab630c92a
Luke Klinker <lklinker1@gmail.com>
2015-08-19 11:03:07 -0500

    add embedded tweet card to the tweet view so that you can see them on the base page of the profile
    Former-commit-id: af301c56ae7e9773471aa080b156b964e435137e

--- src/main/java/com/klinker/android/twitter_l/data/TweetView.java
+++ src/main/java/com/klinker/android/twitter_l/data/TweetView.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
+import android.support.v7.widget.CardView;
 import android.text.Html;
 import android.text.Spannable;
 import android.util.Pair;
@@ -63,6 +64,7 @@ public class TweetView {
     String users;
     String gifUrl;
     boolean isConvo = false;
+    int embeddedTweetMinHeight = 0;
 
     // layout components
     TextView nameTv;
@@ -76,6 +78,7 @@ public class TweetView {
     TextView screenTV;
     FrameLayout imageHolder;
     ImageView isAConvo;
+    CardView embeddedTweet;
 
     public TweetView(Context context) {
         this.context = context;
@@ -196,6 +199,7 @@ public class TweetView {
         screenTV = (TextView) v.findViewById(R.id.screenname);
         imageHolder = (FrameLayout) v.findViewById(R.id.picture_holder);
         isAConvo = (ImageView) v.findViewById(R.id.is_a_conversation);
+        embeddedTweet = (CardView) v.findViewById(R.id.embedded_tweet_card);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             imageIv.setClipToOutline(true);
@@ -207,6 +211,9 @@ public class TweetView {
         screenTV.setTextSize(settings.textSize - 2);
         timeTv.setTextSize(settings.textSize - 3);
         retweeterTv.setTextSize(settings.textSize - 3);
+
+        embeddedTweetMinHeight = Utils.toDP(140, context);
+        embeddedTweet.setMinimumHeight(embeddedTweetMinHeight);
     }
 
     private void bindData() {
@@ -454,5 +461,56 @@ public class TweetView {
 
         TextUtils.linkifyText(context, tweetTv, backgroundLayout, true, otherUrl, false);
         TextUtils.linkifyText(context, retweeterTv, backgroundLayout, true, "", false);
+
+        if (otherUrl != null && otherUrl.contains("/status/") &&
+                embeddedTweet.getChildCount() == 0) {
+            loadEmbeddedTweet(otherUrl);
+        }
+    }
+
+    public void loadEmbeddedTweet(final String otherUrls) {
+
+        embeddedTweet.setVisibility(View.VISIBLE);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Long embeddedId = 0l;
+                for (String u : otherUrls.split(" ")) {
+                    if (u.contains("/status/")) {
+                        embeddedId = TweetLinkUtils.getTweetIdFromLink(u);
+                        break;
+                    }
+                }
+
+                if (embeddedId != 0l) {
+                    Status status = null;
+
+                    try {
+                        status = Utils.getTwitter(context, settings).showStatus(embeddedId);
+                    } catch (Exception e) {
+                        status = null;
+                    }
+
+                    final Status embedded = status;
+
+                    if (status != null) {
+                        ((Activity) context).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TweetView v = new TweetView(context, embedded);
+                                v.setCurrentUser(AppSettings.getInstance(context).myScreenName);
+                                v.setSmallImage(true);
+
+                                embeddedTweet.removeAllViews();
+                                embeddedTweet.addView(v.getView());
+
+                                embeddedTweet.setMinimumHeight(0);
+                            }
+                        });
+                    }
+                }
+            }
+        }).start();
     }
 }
