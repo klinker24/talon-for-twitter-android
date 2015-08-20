f29dfe5dd2d0505d9d94a9b2fb84a0da3a16305e
Luke Klinker <lklinker1@gmail.com>
2015-08-20 07:44:30 -0500

    fix merge
    Former-commit-id: af5ede6a6f3e832238a9a36e6cab1259630d7e0e

--- wear/src/main/java/com/klinker/android/twitter_l/activity/WearActivity.java
+++ wear/src/main/java/com/klinker/android/twitter_l/activity/WearActivity.java
@@ -123,27 +123,4 @@ public class WearActivity extends WearTransactionActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private static final int SPEECH_REQUEST_CODE = 101;
-
-    public void startSpeechRequest() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-        startActivityForResult(intent, SPEECH_REQUEST_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            String spokenText = results.get(0);
-
-            sendComposeRequest(spokenText);
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
 }
