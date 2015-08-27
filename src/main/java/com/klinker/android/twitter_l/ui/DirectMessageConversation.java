a284378e9541d036c199787301fc706fca96b2b2
Luke Klinker <lklinker1@gmail.com>
2015-08-27 16:31:12 -0500

    don't allow user to insert image into dm
    Former-commit-id: 9c5950f4a3b2d0be8c9e6e1b7df9a78e48138138

--- src/main/java/com/klinker/android/twitter_l/ui/DirectMessageConversation.java
+++ src/main/java/com/klinker/android/twitter_l/ui/DirectMessageConversation.java
@@ -243,35 +243,6 @@ public class DirectMessageConversation extends AppCompatActivity {
         }
     }
 
-    public void setUpWindow() {
-
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND,
-                WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-
-        // Params for the window.
-        // You can easily set the alpha and the dim behind the window from here
-        WindowManager.LayoutParams params = getWindow().getAttributes();
-        params.alpha = 1.0f;    // lower than one makes it more transparent
-        params.dimAmount = .75f;  // set it higher if you want to dim behind the window
-        getWindow().setAttributes(params);
-
-        // Gets the display size so that you can set the window to a percent of that
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-
-        // You could also easily used an integer value from the shared preferences to set the percent
-        if (height > width) {
-            getWindow().setLayout((int) (width * .9), (int) (height * .8));
-        } else {
-            getWindow().setLayout((int) (width * .7), (int) (height * .8));
-        }
-
-    }
-
     public int toDP(int px) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, getResources().getDisplayMetrics());
     }
@@ -433,14 +404,6 @@ public class DirectMessageConversation extends AppCompatActivity {
         }
 
         super.onPause();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.dm_conversation, menu);
-
-        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
