7d8cd59fc471bd46d135211c59a1e61f1afc1979
Luke Klinker <lklinker1@gmail.com>
2015-12-22 13:22:56 -0600

    follow the list fragments refresh pattern
    Former-commit-id: 08a3819c973674a8369cf72a096f9d412603b65f

--- src/main/java/com/klinker/android/twitter_l/services/ListRefreshService.java
+++ src/main/java/com/klinker/android/twitter_l/services/ListRefreshService.java
@@ -10,6 +10,7 @@ import com.klinker.android.twitter_l.adapters.TimelinePagerAdapter;
 import com.klinker.android.twitter_l.data.sq_lite.ListDataSource;
 import com.klinker.android.twitter_l.settings.AppSettings;
 import com.klinker.android.twitter_l.ui.MainActivity;
+import com.klinker.android.twitter_l.ui.drawer_activities.DrawerActivity;
 import com.klinker.android.twitter_l.utils.Utils;
 
 import java.util.ArrayList;
@@ -70,51 +71,27 @@ public class ListRefreshService extends IntentService {
                 }
 
                 Twitter twitter = Utils.getTwitter(context, settings);
-                ListDataSource dataSource = ListDataSource.getInstance(context);
-                List<Status> statuses = new ArrayList<Status>();
+
+                long[] lastId = ListDataSource.getInstance(context).getLastIds(listId);
+
+                final List<twitter4j.Status> statuses = new ArrayList<twitter4j.Status>();
 
                 boolean foundStatus = false;
 
                 Paging paging = new Paging(1, 200);
 
-                long[] lastId = null;
-                long id;
-                try {
-                    lastId = dataSource.getLastIds(listId);
-                    id = lastId[1];
-                } catch (Exception e) {
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException i) {
-
-                    }
-                    ListRefreshService.isRunning = false;
-                    return;
+                if (lastId[0] > 0) {
+                    paging.setSinceId(lastId[0]);
                 }
 
-                if (id == 0) {
-                    id = 1;
-                }
+                for (int i = 0; i < DrawerActivity.settings.maxTweetsRefresh; i++) {
 
-                try {
-                    paging.setSinceId(id);
-                } catch (Exception e) {
-                    paging.setSinceId(1l);
-                }
-
-                for (int i = 0; i < settings.maxTweetsRefresh; i++) {
                     try {
                         if (!foundStatus) {
                             paging.setPage(i + 1);
                             List<Status> list = twitter.getUserListStatuses(listId, paging);
+
                             statuses.addAll(list);
-
-                            if (statuses.size() <= 1 || statuses.get(statuses.size() - 1).getId() == lastId[0]) {
-                                foundStatus = true;
-                            } else {
-                                foundStatus = false;
-                            }
-
                         }
                     } catch (Exception e) {
                         // the page doesn't exist
@@ -124,22 +101,8 @@ public class ListRefreshService extends IntentService {
                     }
                 }
 
-                // hash set to check for duplicates I guess
-                HashSet hs = new HashSet();
-                hs.addAll(statuses);
-                statuses.clear();
-                statuses.addAll(hs);
-
-                Long currentTime = Calendar.getInstance().getTimeInMillis();
-                if (currentTime - sharedPrefs.getLong("last_list_insert_" + listId, 0l) < 10000) {
-                    sendBroadcast(new Intent("com.klinker.android.twitter.LIST_REFRESHED").putExtra("number_new", 0));
-                    ListRefreshService.isRunning = false;
-                    return;
-                } else {
-                    sharedPrefs.edit().putLong("last_list_insert_" + listId, currentTime).commit();
-                }
-
-                int inserted = ListDataSource.getInstance(context).insertTweets(statuses, listId);
+                ListDataSource dataSource = ListDataSource.getInstance(context);
+                dataSource.insertTweets(statuses, listId);
 
                 if (!intent.getBooleanExtra("on_start_refresh", false)) {
                     sharedPrefs.edit().putBoolean("refresh_me_list_" + listId, true).commit();
