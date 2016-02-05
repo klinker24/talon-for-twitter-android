fed7aa5d8835fe7b56702f85e69f1d374e7779b7
Luke Klinker <lklinker1@gmail.com>
2016-02-04 21:23:37 -0600

    added support for supporter in app billing transactions from settings
    Former-commit-id: aded1602fbc22474acddf13bd344d0a5cc2d1945

--- src/main/java/com/klinker/android/twitter_l/settings/MainPrefFrag.java
+++ src/main/java/com/klinker/android/twitter_l/settings/MainPrefFrag.java
@@ -15,6 +15,7 @@ package com.klinker.android.twitter_l.settings;
  * limitations under the License.
  */
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
@@ -22,6 +23,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,7 +31,8 @@ import android.widget.ListView;
 import com.klinker.android.twitter_l.R;
 import com.klinker.android.twitter_l.utils.XmlFaqUtils;
 
-public class MainPrefFrag extends PreferenceFragment {
+public class MainPrefFrag extends InAppBillingPreferenceFragment {
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,7 +64,8 @@ public class MainPrefFrag extends PreferenceFragment {
             "advanced_settings",
             "main_pages_and_drawer",
             "memory_management",
-            "get_help"
+            "get_help",
+            "become_supporter"
     };
 
     public void setClicks() {
@@ -72,11 +76,14 @@ public class MainPrefFrag extends PreferenceFragment {
             p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    if (!titles[num].equals("get_help")) {
+                    if (titles[num].equals("become_supporter")) {
+                        showSupporterDialog();
+                    } else if (!titles[num].equals("get_help")) {
                         showSettings(num, preference.getTitle().toString());
                     } else {
                         showGetHelp();
                     }
+
                     return false;
                 }
             });
@@ -93,5 +100,32 @@ public class MainPrefFrag extends PreferenceFragment {
 
     private void showGetHelp() {
         XmlFaqUtils.showFaqDialog(getActivity());
+    }
+
+    private void showSupporterDialog() {
+        new AlertDialog.Builder(getActivity())
+                .setTitle("Love Talon?")
+                .setMessage("Talon has been available for almost a year and a half now, can you believe it? A LOT of work goes into this app every single day. I am just one person trying my best to keep up.\n\n" +
+                        "If you are like me and use Talon every day, chances are that you have gotten your $4 out of the app. Consider becoming a 2016 SUPPORTER to help out development!\n\n" +
+                        "I do want to be clear that becoming a supporter doesn't enhance the app or its feature set in any way. This is purely a voluntary contribution if you have enjoyed my work, like I know many of you have.\n\n" +
+                        "Continue enjoying Talon either way, but I love when my users show me their support :)")
+                .setPositiveButton("$10", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        start2016SupporterPurchase("10");
+                    }
+                })
+                .setNeutralButton("$6", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        start2016SupporterPurchase("6");
+                    }
+                })
+                .setNegativeButton("$3", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        start2016SupporterPurchase("3");
+                    }
+                }).create().show();
     }
 }
