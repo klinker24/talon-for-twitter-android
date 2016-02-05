86a420ffe66877fe8e401d0b89b5deed2aa8dc67
Luke Klinker <lklinker1@gmail.com>
2016-02-04 22:03:05 -0600

    write to the shared preference when it is done with the purchase
    Former-commit-id: cff84a846b205f6814fe8ec94d61704c40046c02

--- src/main/java/com/klinker/android/twitter_l/manipulations/BecomeSupporterPreference.java
+++ src/main/java/com/klinker/android/twitter_l/manipulations/BecomeSupporterPreference.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +25,8 @@ public class BecomeSupporterPreference extends Preference {
         super(context, attributeSet);
     }
 
+    private boolean isSupporter = false;
+
     @Override
     protected View onCreateView(ViewGroup parent) {
         View view = super.onCreateView(parent);
@@ -32,8 +35,10 @@ public class BecomeSupporterPreference extends Preference {
                 Context.MODE_WORLD_READABLE + Context.MODE_WORLD_WRITEABLE);
         TextView textView = (TextView) view.findViewById(android.R.id.title);
 
-        if (sharedPreferences.getBoolean("2016_supporter", false)) {
+        if (sharedPreferences.getBoolean("2016_supporter", true)) {
             setTitle(R.string.are_supporter);
+            setSummary(R.string.are_supporter_summary);
+            isSupporter = true;
         } else {
             textView.setTextSize(19);
             setTitle(R.string.become_supporter);
