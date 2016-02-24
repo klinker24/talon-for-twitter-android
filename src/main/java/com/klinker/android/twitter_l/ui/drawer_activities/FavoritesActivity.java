ea7ffa6d1c365ad2760e42003c09e62eb270ddef
Luke Klinker <lklinker1@gmail.com>
2016-02-24 13:43:36 -0600

    adjust drawer layouts correctly on orientation changes
    Former-commit-id: 248a2f5ba233317647153b7dc72d3080d4609c4c

--- src/main/java/com/klinker/android/twitter_l/ui/drawer_activities/FavoritesActivity.java
+++ src/main/java/com/klinker/android/twitter_l/ui/drawer_activities/FavoritesActivity.java
@@ -253,4 +253,43 @@ public class FavoritesActivity extends DrawerActivity {
         return true;
     }
 
+    private boolean changedConfig = false;
+    private boolean activityActive = true;
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (activityActive) {
+            restartActivity();
+        } else {
+            changedConfig = true;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        activityActive = false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (changedConfig) {
+            restartActivity();
+        }
+
+        activityActive = true;
+        changedConfig = false;
+    }
+
+    private void restartActivity() {
+        overridePendingTransition(0, 0);
+        finish();
+        Intent restart = new Intent(context, FavoritesActivity.class);
+        restart.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        overridePendingTransition(0, 0);
+        startActivity(restart);
+    }
+
 }
