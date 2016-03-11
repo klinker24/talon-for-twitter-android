2ecd504827fbc1e3e4bcaec59605854bf0859015
Luke Klinker <lklinker1@gmail.com>
2016-03-11 07:44:34 -0600

    fix tweeting with images on android N
    Former-commit-id: 3725f6b693218efff4bfed86ee1c621c78b43b59

--- src/main/java/com/klinker/android/twitter_l/utils/IOUtils.java
+++ src/main/java/com/klinker/android/twitter_l/utils/IOUtils.java
@@ -137,6 +137,29 @@ public class IOUtils {
         return Uri.fromFile(file);
     }
 
+    public static Uri getImageContentUri(Context context, File imageFile) {
+        String filePath = imageFile.getAbsolutePath();
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[] { MediaStore.Images.Media._ID },
+                MediaStore.Images.Media.DATA + "=? ",
+                new String[] { filePath }, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+            cursor.close();
+            return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + id);
+        } else {
+            if (imageFile.exists()) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, filePath);
+                return context.getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } else {
+                return null;
+            }
+        }
+    }
+
     public static String getPath(Uri uri, Context context) {
         String[] filePathColumn = {MediaStore.Images.Media.DATA};
         String filePath = null;
