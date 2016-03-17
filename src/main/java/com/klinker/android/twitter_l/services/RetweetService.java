9f0b2b026b6b960d75fa7db1264c87c3a81bd0ed
Luke Klinker <lklinker1@gmail.com>
2016-03-17 19:47:50 -0400

    bundled notifications for mentions and second account mentions
    Former-commit-id: eac8d6341351c7190789cee118db057b8919630e

---
+++ src/main/java/com/klinker/android/twitter_l/services/RetweetService.java
@@ -0,0 +1,57 @@
+package com.klinker.android.twitter_l.services;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.klinker.android.twitter_l.settings.AppSettings;
+import com.klinker.android.twitter_l.utils.Utils;
+
+import twitter4j.Twitter;
+
+public class RetweetService extends IntentService {
+
+    private static final String ARG_ACCOUNT_TO_RETWEET_WITH = "account_num";
+    private static final String ARG_TWEET_ID = "tweet_id";
+    private static final String ARG_NOTIFICATION_ID = "notification_id";
+
+    public static Intent getIntent(Context callingContext, int accountNumberToRetweetWith, long tweetId, int notificationId) {
+        Intent retweet = new Intent(callingContext, RetweetService.class);
+        retweet.putExtra(ARG_ACCOUNT_TO_RETWEET_WITH, accountNumberToRetweetWith);
+        retweet.putExtra(ARG_TWEET_ID, tweetId);
+        retweet.putExtra(ARG_NOTIFICATION_ID, notificationId);
+
+        return retweet;
+    }
+
+    public RetweetService() {
+        super("RetweetService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        int accountToFavoriteWith = intent.getIntExtra(ARG_ACCOUNT_TO_RETWEET_WITH, 1);
+        long tweetId = intent.getLongExtra(ARG_TWEET_ID, 1);
+        int notificationId = intent.getIntExtra(ARG_NOTIFICATION_ID, 1);
+
+        Twitter twitter;
+        if (accountToFavoriteWith == AppSettings.getInstance(this).currentAccount) {
+            twitter = Utils.getTwitter(this, AppSettings.getInstance(this));
+        } else {
+            twitter = Utils.getSecondTwitter(this);
+        }
+
+        try {
+            twitter.retweetStatus(tweetId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        notificationManager.cancel(notificationId);
+    }
+}
