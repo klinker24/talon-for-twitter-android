b6b2deb4d989b692d0e60d1374a1de86772708ee
Luke Klinker <lklinker1@gmail.com>
2016-03-19 10:45:15 -0500

    use a redirect to open up the tweet activity so that the main activity doesn't open
    Former-commit-id: 7aa46c9c41b57bf56fc3ff688034d89dc394bcc0

--- src/main/java/com/klinker/android/twitter_l/widget/WidgetService.java
+++ src/main/java/com/klinker/android/twitter_l/widget/WidgetService.java
@@ -236,18 +236,18 @@ class WidgetViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     public Bitmap getCachedPic(String url) {
         try {
-            /*return ImageUtils.getCircleBitmap(Glide.
+            return ImageUtils.getCircleBitmap(Glide.
                     with(mContext).
                     load(url).
                     asBitmap().
                     into(200, 200).
-                    get());*/
-            return Glide.with(mContext)
+                    get());
+            /*return Glide.with(mContext)
                     .load(url)
                     .asBitmap()
                     .transform(new CircleBitmapTransform(mContext))
                     .into(200,200)
-                    .get();
+                    .get();*/
         } catch (Exception e) {
             return null;
         }
