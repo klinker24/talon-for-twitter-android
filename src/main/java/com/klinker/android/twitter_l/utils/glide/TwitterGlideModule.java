c108753aa9a3cc181f4ae41a9c08d2c85306cbc7
Luke Klinker <lklinker1@gmail.com>
2016-03-30 10:55:07 -0500

    add option for higher quality images
    Former-commit-id: 72840029fbe7b7621199560c497ffbe86094e96f

--- src/main/java/com/klinker/android/twitter_l/utils/glide/TwitterGlideModule.java
+++ src/main/java/com/klinker/android/twitter_l/utils/glide/TwitterGlideModule.java
@@ -4,14 +4,19 @@ import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.GlideModule;
+import com.klinker.android.twitter_l.settings.AppSettings;
 
 import java.io.InputStream;
 
 public class TwitterGlideModule implements GlideModule {
     @Override public void applyOptions(Context context, GlideBuilder builder) {
-
+        AppSettings settings = AppSettings.getInstance(context);
+        if (settings.higherQualityImages) {
+            builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+        }
     }
 
     @Override public void registerComponents(Context context, Glide glide) {
