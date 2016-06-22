fd357f8bbf04c8b9c34d751fda43f8c9b964b76a
Luke Klinker <lklinker1@gmail.com>
2016-06-21 21:58:11 -0500

    some bug fixes
    Former-commit-id: 757bdb8f3fc9d509f535777c9e3d463cc20e208c

--- src/main/java/com/klinker/android/twitter_l/settings/AppSettings.java
+++ src/main/java/com/klinker/android/twitter_l/settings/AppSettings.java
@@ -419,7 +419,7 @@ public class AppSettings {
         autoplay = Integer.parseInt(sharedPrefs.getString("autoplay", AUTOPLAY_WIFI + ""));
 
         String widgetAccount = sharedPrefs.getString("widget_account", "").replace("@", "");
-        if (widgetAccount.equals(myScreenName.replace("@",""))) {
+        if (widgetAccount.equals(myScreenName.replace("@","")) || widgetAccount.equals("")) {
             widgetAccountNum = currentAccount;
         } else {
             if (currentAccount == 1) {
