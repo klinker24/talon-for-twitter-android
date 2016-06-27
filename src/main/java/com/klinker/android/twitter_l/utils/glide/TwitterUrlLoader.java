7080337b1b04a30ac16b797090da352c01ee0585
Luke Klinker <lklinker1@gmail.com>
2016-06-27 18:29:56 -0500

    use okhttp with twitter4j and glide
    Former-commit-id: 89727dbd1c340b0ae37de395f428fe6c002cac14

--- src/main/java/com/klinker/android/twitter_l/utils/glide/TwitterUrlLoader.java
+++ src/main/java/com/klinker/android/twitter_l/utils/glide/TwitterUrlLoader.java
@@ -2,6 +2,8 @@ package com.klinker.android.twitter_l.utils.glide;
 
 import android.content.Context;
 
+import com.bumptech.glide.integration.okhttp3.OkHttpStreamFetcher;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.data.DataFetcher;
 import com.bumptech.glide.load.model.GenericLoaderFactory;
 import com.bumptech.glide.load.model.GlideUrl;
@@ -10,28 +12,53 @@ import com.bumptech.glide.load.model.ModelLoaderFactory;
 
 import java.io.InputStream;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+
 public class TwitterUrlLoader implements ModelLoader<GlideUrl, InputStream> {
 
-    public static class Factory implements ModelLoaderFactory<GlideUrl, InputStream> {
-        @Override
-        public ModelLoader<GlideUrl, InputStream> build(Context context, GenericLoaderFactory factories) {
-            return new TwitterUrlLoader(context);
+    public static class Factory extends OkHttpUrlLoader.Factory {
+
+        private static volatile Call.Factory internalClient;
+        private Call.Factory client;
+
+        public Factory() {
+            this(getInternalClient());
+        }
+
+        public Factory(Call.Factory client) {
+            super(client);
+            this.client = client;
         }
 
         @Override
-        public void teardown() {
+        public ModelLoader<GlideUrl, InputStream> build(Context context, GenericLoaderFactory factories) {
+            return new TwitterUrlLoader(client, context);
+        }
 
+        private static Call.Factory getInternalClient() {
+            if (internalClient == null) {
+                synchronized (OkHttpUrlLoader.Factory.class) {
+                    if (internalClient == null) {
+                        internalClient = new OkHttpClient();
+                    }
+                }
+            }
+            return internalClient;
         }
     }
 
     private Context context;
+    private final Call.Factory client;
 
-    public TwitterUrlLoader(Context context) {
+    public TwitterUrlLoader(Call.Factory client, Context context) {
         this.context = context;
+        this.client = client;
     }
 
     @Override
     public DataFetcher<InputStream> getResourceFetcher(GlideUrl model, int width, int height) {
-        return new TwitterStreamFetcher(context, model);
+        return new TwitterStreamFetcher(context, client, model);
+        //return new OkHttpStreamFetcher(client, model);
     }
 }
