2f02f32fd8d2f558e74ab2093d0d7c713fc625b7
Luke Klinker <lklinker1@gmail.com>
2016-07-12 06:43:14 -0500

    use the easy video player library for the peek videos

---
+++ src/main/java/com/klinker/android/twitter_l/utils/EasyVideoCallbackWrapper.java
@@ -0,0 +1,57 @@
+package com.klinker.android.twitter_l.utils;
+
+import android.net.Uri;
+
+import com.afollestad.easyvideoplayer.EasyVideoCallback;
+import com.afollestad.easyvideoplayer.EasyVideoPlayer;
+
+/**
+ * Created by luke on 7/12/16.
+ */
+
+public class EasyVideoCallbackWrapper implements EasyVideoCallback {
+    @Override
+    public void onStarted(EasyVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onPaused(EasyVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onPreparing(EasyVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onPrepared(EasyVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onBuffering(int percent) {
+
+    }
+
+    @Override
+    public void onError(EasyVideoPlayer player, Exception e) {
+
+    }
+
+    @Override
+    public void onCompletion(EasyVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onRetry(EasyVideoPlayer player, Uri source) {
+
+    }
+
+    @Override
+    public void onSubmit(EasyVideoPlayer player, Uri source) {
+
+    }
+}
