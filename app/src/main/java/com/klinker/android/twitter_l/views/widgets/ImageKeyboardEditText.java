791caa14a7e90e888b234ca1a210dc10da60dc12
Luke Klinker <lklinker1@gmail.com>
2016-10-20 20:28:05 -0500

    improve image keyboard

--- app/src/main/java/com/klinker/android/twitter_l/views/widgets/ImageKeyboardEditText.java
+++ app/src/main/java/com/klinker/android/twitter_l/views/widgets/ImageKeyboardEditText.java
@@ -3,8 +3,10 @@ package com.klinker.android.twitter_l.views.widgets;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v13.view.inputmethod.EditorInfoCompat;
 import android.support.v13.view.inputmethod.InputConnectionCompat;
 import android.support.v13.view.inputmethod.InputContentInfoCompat;
+import android.support.v4.os.BuildCompat;
 import android.util.AttributeSet;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
@@ -33,17 +35,26 @@ public class ImageKeyboardEditText extends FontPrefEditText {
     @Override
     public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
         InputConnection con = super.onCreateInputConnection(outAttrs);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            outAttrs.contentMimeTypes = new String[]{ "image/gif", "image/jpeg",
-                    "image/jpg", "image/png", "video/mp4" };
-        }
+        EditorInfoCompat.setContentMimeTypes(outAttrs, new String[] { "image/gif", "image/png" });
 
         return InputConnectionCompat.createWrapper(con, outAttrs, new InputConnectionCompat.OnCommitContentListener() {
             @Override
             public boolean onCommitContent(InputContentInfoCompat inputContentInfo, int flags, Bundle opts) {
                 if (commitContentListener != null) {
-                    return commitContentListener.onCommitContent(inputContentInfo, flags, opts);
+                    if (BuildCompat.isAtLeastNMR1() &&
+                            (flags & InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+                        try {
+                            inputContentInfo.requestPermission();
+                        } catch (Exception e) {
+                            return false;
+                        }
+                    }
+
+                    commitContentListener.onCommitContent(
+                            inputContentInfo, flags, opts
+                    );
+
+                    return true;
                 } else {
                     return false;
                 }
