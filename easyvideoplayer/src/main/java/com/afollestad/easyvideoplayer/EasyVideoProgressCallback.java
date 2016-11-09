d9dcfa1cb5c50f55e440a1ff864ed7b48d2d8409
Luke Klinker <lklinker1@gmail.com>
2016-11-08 19:51:54 -0600

    update easy video player and material camera dependencies

--- easyvideoplayer/src/main/java/com/afollestad/easyvideoplayer/EasyVideoProgressCallback.java
+++
@@ -1,9 +0,0 @@
-package com.afollestad.easyvideoplayer;
-
-/**
- * @author Aidan Follestad (afollestad)
- */
-public interface EasyVideoProgressCallback {
-
-    void onVideoProgressUpdate(int position, int duration);
-}
--- material-camera/src/main/java/com/afollestad/materialcamera/ICallback.java
+++
@@ -1,9 +0,0 @@
-package com.afollestad.materialcamera;
-
-public interface ICallback {
-    /**
-     * It is called when the background operation completes.
-     * If the operation is successful, {@code exception} will be {@code null}.
-     */
-    void done(Exception exception);
-}
