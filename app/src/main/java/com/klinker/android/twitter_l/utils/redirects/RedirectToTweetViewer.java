b208fa874a0b634b79d6e89357a9b7005b5fc9b7
Luke Klinker <lklinker1@gmail.com>
2017-03-19 15:09:26 -0500

    change to how tweet ids are handled in notifications

--- app/src/main/java/com/klinker/android/twitter_l/utils/redirects/RedirectToTweetViewer.java
+++ app/src/main/java/com/klinker/android/twitter_l/utils/redirects/RedirectToTweetViewer.java
@@ -19,6 +19,12 @@ public class RedirectToTweetViewer extends AppCompatActivity {
 
         Intent tweet = new Intent(this, TweetActivity.class);
         tweet.putExtras(getIntent());
+
+        long forcedTweetId = getIntent().getLongExtra("forced_tweet_id", -1);
+        if (forcedTweetId != -1) {
+            tweet.putExtra("tweetid", forcedTweetId);
+        }
+        
         tweet.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
         TweetActivity.applyDragDismissBundle(this, tweet);
