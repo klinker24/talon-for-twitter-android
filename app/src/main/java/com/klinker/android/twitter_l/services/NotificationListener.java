121345f9ba8ed1c67a1290f4b6b643f43a705dff
Luke Klinker <lklinker1@gmail.com>
2017-05-07 18:17:49 -0500

    add notification listener service to update the activity utils

---
+++ app/src/main/java/com/klinker/android/twitter_l/services/NotificationListener.java
@@ -0,0 +1,55 @@
+package com.klinker.android.twitter_l.services;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import com.klinker.android.twitter_l.settings.AppSettings;
+import com.klinker.android.twitter_l.utils.ActivityUtils;
+
+public class NotificationListener extends NotificationListenerService {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    public void onListenerConnected() {
+        Log.v("TalonPushIntercept", "listener connected");
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        Log.v("TalonPushIntercept", "notification posted");
+
+        if (AppSettings.getInstance(this).interceptTwitterNotifications &&
+                sbn.getPackageName().equals("com.twitter.android")) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    ActivityUtils utils = new ActivityUtils(NotificationListener.this);
+                    boolean newActivity = utils.refreshActivity();
+
+                    if (newActivity) {
+                        utils.postNotification();
+                    }
+                }
+            }).start();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                cancelNotification(sbn.getKey());
+            } else {
+                cancelNotification(sbn.getPackageName(), sbn.getTag(), sbn.getId());
+            }
+        }
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+
+    }
+}
