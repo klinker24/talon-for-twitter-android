ab6bfc51b05535b42268a3ef141790abcae061df
Luke Klinker <lklinker1@gmail.com>
2017-06-09 07:58:41 -0500

    quick settings tile to compose a new tweet

---
+++ app/src/main/java/com/klinker/android/twitter_l/services/ComposeTileService.java
@@ -0,0 +1,24 @@
+package com.klinker.android.twitter_l.services;
+
+import android.content.Intent;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.support.annotation.RequiresApi;
+
+import com.klinker.android.twitter_l.activities.compose.LauncherCompose;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class ComposeTileService extends TileService {
+
+    @Override
+    public void onClick() {
+        Intent compose = new Intent(this, LauncherCompose.class);
+        startActivityAndCollapse(compose);
+    }
+
+    @Override
+    public void onStartListening() {
+        getQsTile().setState(Tile.STATE_ACTIVE);
+    }
+}
