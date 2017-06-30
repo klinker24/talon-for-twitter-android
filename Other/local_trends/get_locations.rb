5dc995650c9e5576aab663511607f7e87c5bcf6e
Luke Klinker <lklinker1@gmail.com>
2017-06-30 09:16:34 -0700

    update the local trends program to be a ruby script

--- Other/local_trends/get_locations.rb
+++ Other/local_trends/get_locations.rb
@@ -7,6 +7,59 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = "***REMOVED***"
 end
 
-client.trends_available.each do |location|
-	puts "#{location.name}, #{location.country} woeid: #{location.woeid}"
+locations = client.trends_available
+
+puts "========== Countries String Array =========="
+puts ""
+puts "<string-array name=\"countries\">"
+
+countries = locations.uniq { |x| x.country }
+countries = countries.sort { |x, y| x.country <=> y.country }
+
+countries.each do |country|
+	puts "    <item>#{country.country}</item>"
 end
+
+puts "</string-array>"
+
+puts "\n\n"
+puts "========== Cities by Country =========="
+puts ""
+
+countries.each do |country|
+	cities = locations.select { |location| location.country == country.country }
+	cities = cities.sort { |x, y| x.name <=> y.name }
+
+	name = country.country.downcase.gsub ' ', '_'
+	puts "public static String[][] #{name} = {"
+	cities.each do |city|
+		if (city.name == country.country)
+			puts "    { \"All Cities\", \"#{city.woeid}\" },"
+		else
+			puts "    { \"#{city.name}\", \"#{city.woeid}\" },"
+		end
+	end
+	puts "};"
+	puts "\n"
+end
+
+puts "\n\n"
+puts "========== Return Statement to Convert String Array to Object =========="
+puts ""
+
+puts "public static String[][] getArray(String countryName) {"
+
+countries.each do |country|
+	puts "    } else if (countryName.equals(\"#{country.country}\")) {"
+
+	name = country.country.downcase.gsub ' ', '_'
+	puts "        return #{name};"
+end
+
+puts "    }"
+puts "    return null;"
+puts "}"
+
+# locations.each do |location|
+# 	puts "#{location.name}, #{location.country} woeid: #{location.woeid}"
+# end
