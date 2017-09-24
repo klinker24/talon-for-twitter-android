a76fb21110cbdf457e7a64651cb275ab4be4eea5
Luke Klinker <lklinker1@gmail.com>
2017-09-24 14:00:29 -0500

    add analytics events to the rate it dialog

--- app/src/main/java/com/klinker/android/twitter_l/utils/AnalyticsHelper.java
+++ app/src/main/java/com/klinker/android/twitter_l/utils/AnalyticsHelper.java
@@ -8,11 +8,18 @@ import com.klinker.android.twitter_l.settings.AppSettings;
 
 public class AnalyticsHelper {
 
+    // LOGIN EVENTS
     private static final String START_LOGIN = "START_LOGIN";
     private static final String LOGIN_TO_TWITTER = "LOGIN_TO_TWITTER";
     private static final String FINISH_LOGIN_TO_TWITTER = "FINISH_LOGIN_TO_TWITTER";
     private static final String LOGIN_DOWNLOAD_TWEETS = "LOGIN_DOWNLOADED_TWEETS";
     private static final String FINISH_LOGIN = "FINISH_LOGIN";
+
+    // RATE IT EVENTS
+    private static final String SHOW_RATE_IT_PROMPT = "SHOW_RATE_IT_PROMPT";
+    private static final String RATE_IT_ON_PLAY_STORE = "RATE_IT_ON_PLAY_STORE";
+
+    // GENERAL LOGGING
     private static final String ERROR_LOADING_FROM_NOTIFICATION = "ERROR_LOADING_FROM_NOTIFICATION";
 
     private static void logEvent(Context context, String event) {
@@ -34,7 +41,7 @@ public class AnalyticsHelper {
     }
 
     public static void finishLoginToTwitter(Context context) {
-        logEvent(context, LOGIN_TO_TWITTER);
+        logEvent(context, FINISH_LOGIN_TO_TWITTER);
     }
 
     public static void downloadTweets(Context context) {
@@ -43,6 +50,14 @@ public class AnalyticsHelper {
 
     public static void finishLogin(Context context) {
         logEvent(context, FINISH_LOGIN);
+    }
+
+    public static void showRateItPrompt(Context context) {
+        logEvent(context, SHOW_RATE_IT_PROMPT);
+    }
+
+    public static void rateItOnPlayStore(Context context) {
+        logEvent(context, RATE_IT_ON_PLAY_STORE);
     }
 
     public static void errorLoadingTweetFromNotification(Context context, String errorMessage) {
