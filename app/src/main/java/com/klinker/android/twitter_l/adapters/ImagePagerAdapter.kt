735c348a39b006454291d34874aa121abc403ad3
Luke Klinker <lklinker1@gmail.com>
2017-09-28 13:38:08 -0500

    implement the save, share, and info buttons on the tweet images

--- app/src/main/java/com/klinker/android/twitter_l/adapters/ImagePagerAdapter.kt
+++ app/src/main/java/com/klinker/android/twitter_l/adapters/ImagePagerAdapter.kt
@@ -1,6 +1,5 @@
 package com.klinker.android.twitter_l.adapters
 
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 
@@ -8,13 +7,13 @@ import com.klinker.android.twitter_l.activities.media_viewer.image.ImageFragment
 
 class ImagePagerAdapter(fm: FragmentManager, private val urls: Array<String>) : FragmentPagerAdapter(fm) {
 
-    private val fragments = mutableListOf<Fragment>()
+    private val fragments = mutableListOf<ImageFragment>()
 
     init {
         urls.indices.mapTo(fragments) { ImageFragment.getInstance(it, urls[it]) }
     }
 
-    override fun getItem(i: Int): Fragment {
+    override fun getItem(i: Int): ImageFragment {
         return fragments[i]
     }
 
