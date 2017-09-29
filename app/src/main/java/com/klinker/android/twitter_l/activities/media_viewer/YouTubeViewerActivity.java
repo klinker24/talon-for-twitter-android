8790d8febe5b9fbbac650dc629107239f52c992f
Luke Klinker <lklinker1@gmail.com>
2017-09-29 10:26:35 -0500

    fix the youtube viewer

--- app/src/main/java/com/klinker/android/twitter_l/activities/media_viewer/YouTubeViewerActivity.java
+++ app/src/main/java/com/klinker/android/twitter_l/activities/media_viewer/YouTubeViewerActivity.java
@@ -50,14 +50,6 @@ public class YouTubeViewerActivity extends AppCompatActivity {
     
     public Context context;
     public String url;
-    
-    private ImageButton share;
-    private ImageButton download;
-    private ImageButton info;
-
-    private BottomSheetLayout bottomSheet;
-
-    private VideoFragment videoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,38 +77,10 @@ public class YouTubeViewerActivity extends AppCompatActivity {
 
         setContentView(R.layout.video_view_activity);
 
-        download = (ImageButton) findViewById(R.id.save_button);
-        info = (ImageButton) findViewById(R.id.info_button);
-        share = (ImageButton) findViewById(R.id.share_button);
-
         YouTubePlayerFragment fragment = TweetYouTubeFragment.getInstance(context, url);
         getFragmentManager().beginTransaction()
                 .add(R.id.fragment, fragment)
                 .commit();
-
-        findViewById(R.id.buttons_layout).setVisibility(View.GONE);
-        getSupportActionBar().hide();
-
-        bottomSheet = (BottomSheetLayout) findViewById(R.id.bottomsheet);
-
-        download.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                downloadVideo();
-            }
-        });
-        info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showInfo();
-            }
-        });
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                shareVideo();
-            }
-        });
 
         android.support.v7.app.ActionBar ab = getSupportActionBar();
         if (ab != null) {
@@ -126,144 +90,11 @@ public class YouTubeViewerActivity extends AppCompatActivity {
             ab.setDisplayShowHomeEnabled(true);
             ab.setTitle("");
             ab.setIcon(transparent);
+            ab.hide();
         }
 
-        final long tweetId = getIntent().getLongExtra("tweet_id", 0);
-        if (tweetId != 0) {
-            prepareInfo(tweetId);
-        } else {
-            findViewById(R.id.buttons_layout).setVisibility(View.GONE);
-        }
-
+        findViewById(R.id.fragment).setPadding(0,0,0,0);
         new NavBarOverlayLayout(this).show();
-    }
-
-    private void downloadVideo() {
-        if (videoFragment != null && videoFragment.getLoadedVideoLink().contains(".m3u8")) {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/+LukeKlinker/posts/4ZTM55gKVPi"));
-            startActivity(browserIntent);
-            return;
-        }
-
-        new TimeoutThread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    NotificationCompat.Builder mBuilder =
-                            new NotificationCompat.Builder(context, NotificationChannelUtil.MEDIA_DOWNLOAD_CHANNEL)
-                                    .setSmallIcon(R.drawable.ic_stat_icon)
-                                    .setTicker(context.getResources().getString(R.string.downloading) + "...")
-                                    .setContentTitle(context.getResources().getString(R.string.app_name))
-                                    .setContentText(context.getResources().getString(R.string.saving_video) + "...")
-                                    .setProgress(100, 100, true)
-                                    .setOngoing(true);
-
-                    NotificationManager mNotificationManager =
-                            (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                    mNotificationManager.notify(6, mBuilder.build());
-
-                    Intent intent = new Intent();
-                    if (videoFragment != null) {
-                        Uri uri = IOUtils.saveVideo(videoFragment.getLoadedVideoLink());
-
-                        String root = Environment.getExternalStorageDirectory().toString();
-                        File myDir = new File(root + "/Talon");
-                        File file = new File(myDir, uri.getLastPathSegment());
-
-                        try {
-                            uri = FileProvider.getUriForFile(context,
-                                    BuildConfig.APPLICATION_ID + ".provider", file);
-                        } catch (Exception e) {
-
-                        }
-
-                        intent.setAction(Intent.ACTION_VIEW);
-                        intent.setDataAndType(uri, "surfaceView/*");
-                    }
-
-                    PendingIntent pending = PendingIntent.getActivity(context, 91, intent, 0);
-
-                    mBuilder =
-                            new NotificationCompat.Builder(context, NotificationChannelUtil.MEDIA_DOWNLOAD_CHANNEL)
-                                    .setContentIntent(pending)
-                                    .setSmallIcon(R.drawable.ic_stat_icon)
-                                    .setTicker(context.getResources().getString(R.string.saved_video) + "...")
-                                    .setContentTitle(context.getResources().getString(R.string.app_name))
-                                    .setContentText(context.getResources().getString(R.string.saved_video) + "!");
-
-                    mNotificationManager.notify(6, mBuilder.build());
-                } catch (final Exception e) {
-                    e.printStackTrace();
-
-                    ((Activity)context).runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                new PermissionModelUtils(context).showStorageIssue(e);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-
-                    NotificationCompat.Builder mBuilder =
-                            new NotificationCompat.Builder(context, NotificationChannelUtil.MEDIA_DOWNLOAD_CHANNEL)
-                                    .setSmallIcon(R.drawable.ic_stat_icon)
-                                    .setTicker(context.getResources().getString(R.string.error) + "...")
-                                    .setContentTitle(context.getResources().getString(R.string.app_name))
-                                    .setContentText(context.getResources().getString(R.string.error) + "...")
-                                    .setProgress(0, 100, true);
-
-                    NotificationManager mNotificationManager =
-                            (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                    mNotificationManager.notify(6, mBuilder.build());
-                }
-            }
-        }).start();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return true;
-        }
-    }
-
-    private void shareVideo() {
-        Intent share = new Intent(Intent.ACTION_SEND);
-        share.setType("text/plain");
-
-        if (videoFragment != null) {
-            share.putExtra(Intent.EXTRA_TEXT, videoFragment.getLoadedVideoLink());
-        } else {
-            share.putExtra(Intent.EXTRA_TEXT, url);
-        }
-
-        context.startActivity(share);
-    }
-
-    private DetailedTweetView tweetView;
-
-    public void prepareInfo(final long tweetId) {
-        tweetView = DetailedTweetView.create(context, tweetId);
-        tweetView.setShouldShowImage(false);
-    }
-
-    public void showInfo() {
-        View v = tweetView.getView();
-        AppSettings settings = AppSettings.getInstance(this);
-        if (settings.darkTheme || settings.blackTheme) {
-            v.setBackgroundResource(R.color.dark_background);
-        } else {
-            v.setBackgroundResource(R.color.white);
-        }
-
-        bottomSheet.showWithSheetView(v);
     }
 
     @Override
