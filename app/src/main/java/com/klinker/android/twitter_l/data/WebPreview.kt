ac388b8d62732c07c6db5fea9415be690ca66d60
Luke Klinker <lklinker1@gmail.com>
2017-09-30 19:00:18 -0600

    start loading web previews into the timeline UI

--- app/src/main/java/com/klinker/android/twitter_l/data/WebPreview.kt
+++ app/src/main/java/com/klinker/android/twitter_l/data/WebPreview.kt
@@ -1,3 +1,3 @@
 package com.klinker.android.twitter_l.data
 
-class WebPreview(val title: String, val summary: String, val imageUrl: String)
+class WebPreview(val title: String, val summary: String, val imageUrl: String, val webDomain: String, val link: String)
--- app/src/main/java/com/klinker/android/twitter_l/views/WebPreviewCard.kt
+++ app/src/main/java/com/klinker/android/twitter_l/views/WebPreviewCard.kt
@@ -1,22 +1,28 @@
 package com.klinker.android.twitter_l.views
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.CardView
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.klinker.android.twitter_l.R
+import com.klinker.android.twitter_l.activities.BrowserActivity
+import com.klinker.android.twitter_l.activities.media_viewer.VideoViewerActivity
 import com.klinker.android.twitter_l.data.WebPreview
 import com.klinker.android.twitter_l.settings.AppSettings
+import com.klinker.android.twitter_l.utils.WebIntentBuilder
+import com.klinker.android.twitter_l.utils.api_helper.MercuryArticleParserHelper
 
 class WebPreviewCard @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : CardView(context, attrs, defStyleAttr) {
 
     interface OnLoad {
-        fun onLinkLoaded(link: String, preview: WebPreview)
+        fun onLinkLoaded(link: String, preview: WebPreview?)
     }
 
     private var loadedPreview: WebPreview? = null
@@ -28,8 +34,12 @@ class WebPreviewCard @JvmOverloads constructor(
     init {
         LayoutInflater.from(context).inflate(R.layout.card_web_preview, this, true)
 
-        title.textSize = AppSettings.getInstance(context).textSize + 2.toFloat()
+        title.textSize = AppSettings.getInstance(context).textSize + 3.toFloat()
         summary.textSize = AppSettings.getInstance(context).textSize.toFloat()
+
+        setOnClickListener {
+
+        }
     }
 
     fun loadLink(link: String, listener: OnLoad) {
@@ -38,11 +48,44 @@ class WebPreviewCard @JvmOverloads constructor(
             return
         }
 
-        // TODO: Load here
+        MercuryArticleParserHelper.getArticle(link) { webPreview ->
+            listener.onLinkLoaded(link, webPreview)
+            displayPreview(webPreview)
+        }
     }
 
-    fun displayPreview(preview: WebPreview) {
+    fun displayPreview(preview: WebPreview?) {
         loadedPreview = preview
+
+        if (preview != null) {
+            Glide.with(context).load(preview.imageUrl).into(image)
+            title.text = preview.title
+
+            if (preview.summary.isBlank()) {
+                summary.text = preview.webDomain
+            } else {
+                summary.text = preview.summary
+            }
+
+            val link = preview.link
+            setOnClickListener {
+                if (link.contains("/i/web/status/") || link.contains("twitter.com") && link.contains("/moments/")) {
+                    val browser = Intent(context, BrowserActivity::class.java)
+                    browser.putExtra("url", link)
+                    context.startActivity(browser)
+                } else if (link.contains("vine.co/v/")) {
+                    VideoViewerActivity.startActivity(context, 0L, link, "")
+                } else {
+                    WebIntentBuilder(context)
+                            .setUrl(link)
+                            .build().start()
+                }
+            }
+        } else {
+            Glide.clear(image)
+            title.text = "No Preview available"
+            summary.text = ""
+        }
     }
 
     fun clear() {
@@ -51,5 +94,7 @@ class WebPreviewCard @JvmOverloads constructor(
         summary.text = ""
 
         loadedPreview = null
+
+        setOnClickListener { }
     }
 }
