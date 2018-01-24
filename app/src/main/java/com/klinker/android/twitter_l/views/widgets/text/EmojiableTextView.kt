d11c024b42ece66a05493711c5728d315dbab0d0
Luke Klinker <lklinker1@gmail.com>
2018-01-23 21:05:05 -0600

    don't use emoji compat on the names field

--- app/src/main/java/com/klinker/android/twitter_l/views/widgets/text/EmojiableTextView.kt
+++ app/src/main/java/com/klinker/android/twitter_l/views/widgets/text/EmojiableTextView.kt
@@ -5,14 +5,16 @@ import android.support.text.emoji.widget.EmojiTextViewHelper
 import android.support.v7.widget.AppCompatTextView
 import android.text.InputFilter
 import android.util.AttributeSet
+import com.klinker.android.twitter_l.R
 import com.klinker.android.twitter_l.data.EmojiStyle
 import com.klinker.android.twitter_l.settings.AppSettings
 
 open class EmojiableTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
     : AppCompatTextView(context, attrs, defStyleAttr) {
 
+    // for some reason, the flag emojis aren't displaying in people's names
     private val useEmojiCompat: Boolean
-        get() = AppSettings.getInstance(context).emojiStyle != EmojiStyle.DEFAULT
+        get() = AppSettings.getInstance(context).emojiStyle != EmojiStyle.DEFAULT && id != R.id.name
 
     private var helper: EmojiTextViewHelper? = null
     private val emojiHelper: EmojiTextViewHelper
