d92df1afbe932a89df197779ae7ae9bbf0496967
Luke Klinker <lklinker1@gmail.com>
2018-02-15 06:43:06 -0600

    remove feature flag for saving tweets and update the app description

--- app/src/main/java/com/klinker/android/twitter_l/utils/FeatureFlags.java
+++ app/src/main/java/com/klinker/android/twitter_l/utils/FeatureFlags.java
@@ -1,9 +1,7 @@
 package com.klinker.android.twitter_l.utils;
 
-import com.klinker.android.twitter_l.BuildConfig;
-
 public class FeatureFlags {
 
-    public static final boolean SAVED_TWEETS = BuildConfig.DEBUG;
+    public static final boolean SAVED_TWEETS = true;
     
 }
