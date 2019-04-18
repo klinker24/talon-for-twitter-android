cf56332b4bc7a2170e0468781c610139525c9906
Luke Klinker <lklinker1@gmail.com>
2019-04-18 08:56:35 -0500

    provide analytics events for failing lincence check

--- app/src/main/java/com/klinker/android/twitter_l/utils/AnalyticsHelper.java
+++ app/src/main/java/com/klinker/android/twitter_l/utils/AnalyticsHelper.java
@@ -21,6 +21,8 @@ public class AnalyticsHelper {
 
     // GENERAL LOGGING
     private static final String ERROR_LOADING_FROM_NOTIFICATION = "ERROR_LOADING_FROM_NOTIFICATION";
+    private static final String APP_NOT_PURCHASED = "APP_NOT_PURCHASED";
+    private static final String APP_PURCHASED = "APP_PURCHASED";
 
     private static void logEvent(Context context, String event) {
         Bundle bundle = new Bundle();
@@ -64,5 +66,13 @@ public class AnalyticsHelper {
         Bundle bundle = new Bundle();
         bundle.putString("error_message", errorMessage);
         logEvent(context, ERROR_LOADING_FROM_NOTIFICATION);
+    }
+
+    public static void appNotPurchased(Context context) {
+        logEvent(context, APP_NOT_PURCHASED);
+    }
+
+    public static void appPurchased(Context context) {
+        logEvent(context, APP_PURCHASED);
     }
 }
--- app/src/main/java/com/klinker/android/twitter_l/utils/LvlCheck.kt
+++ app/src/main/java/com/klinker/android/twitter_l/utils/LvlCheck.kt
@@ -1,6 +1,7 @@
 package com.klinker.android.twitter_l.utils
 
 import android.content.Context
+import android.preference.PreferenceManager
 import android.util.Log
 import com.github.javiersantos.piracychecker.*
 import com.klinker.android.twitter_l.BuildConfig
@@ -11,6 +12,11 @@ object LvlCheck {
 
     @JvmStatic
     fun check(context: Context) {
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        if (sharedPrefs.getBoolean("was_downloaded_from_store", false)) {
+            return
+        }
+
         val base64 = BuildConfig.LVL_KEY
         if (base64 == "none") {
             return
@@ -20,10 +26,13 @@ object LvlCheck {
             enableGooglePlayLicensing(base64)
             callback {
                 allow {
-                    Log.v("LvlChecker", "app was purchased on the store.")
+                    AnalyticsHelper.appPurchased(context)
+                    sharedPrefs.edit().putBoolean("was_downloaded_from_store", true).commit()
                 }
                 doNotAllow { piracyCheckerError, pirateApp ->
-                    Log.v("LvlChecker", "the app is unlicensed.")
+                    AnalyticsHelper.appNotPurchased(context)
+
+                    // TODO: warn the user that they will be logged out.
                 }
                 onError { error ->
                     Log.v("LvlChecker", "an error occurred during license verification: $error")
