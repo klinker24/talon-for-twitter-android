78ffd81355635d15ee00181574c97867195fc056
Luke Klinker <lklinker1@gmail.com>
2019-04-18 09:04:56 -0500

    only check the license status once per day and allow for retry support on the first failure

--- app/src/main/java/com/klinker/android/twitter_l/utils/LvlCheck.kt
+++ app/src/main/java/com/klinker/android/twitter_l/utils/LvlCheck.kt
@@ -5,16 +5,20 @@ import android.preference.PreferenceManager
 import android.util.Log
 import com.github.javiersantos.piracychecker.*
 import com.klinker.android.twitter_l.BuildConfig
+import java.util.*
 
 
 @Suppress("ConstantConditionIf")
 object LvlCheck {
 
     @JvmStatic
-    fun check(context: Context) {
+    fun check(context: Context, retry: Boolean = true) {
         val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
-        if (sharedPrefs.getBoolean("was_downloaded_from_store", false)) {
+        val oneDayAgo = Date().time - (1000 * 60 * 60 * 24)
+        if (sharedPrefs.getLong("last_licence_check", -1) > oneDayAgo) {
             return
+        } else {
+            sharedPrefs.edit().putLong("last_licence_check", Date().time).commit()
         }
 
         val base64 = BuildConfig.LVL_KEY
@@ -27,12 +31,16 @@ object LvlCheck {
             callback {
                 allow {
                     AnalyticsHelper.appPurchased(context)
-                    sharedPrefs.edit().putBoolean("was_downloaded_from_store", true).commit()
                 }
                 doNotAllow { piracyCheckerError, pirateApp ->
                     AnalyticsHelper.appNotPurchased(context)
 
-                    // TODO: warn the user that they will be logged out.
+                    if (retry) {
+                        LvlCheck.check(context, false)
+                    } else {
+                        // TODO: warn the user that they will be logged out.
+                    }
+
                 }
                 onError { error ->
                     Log.v("LvlChecker", "an error occurred during license verification: $error")
